9b24300333da0f698aead905dd9caa03f6cba748
James A. Petts <jamesapetts@gmail.com>
2020-12-02 15:08:46 +0000

    WIP

--- src/cornerstone-tools-3d/tools/StackScrollTool.ts
+++ src/cornerstone-tools-3d/tools/StackScrollTool.ts
@@ -3,6 +3,10 @@ import { BaseTool } from './base/index.ts';
 // ~~ VTK Viewport
 import { getEnabledElement, imageCache } from './../../index';
 import { vec3 } from 'gl-matrix';
+// @ts-ignore
+import getVolumeActorCorners from '../util/vtkjs/getVolumeActorCorners.ts';
+
+import vtkMatrixBuilder from 'vtk.js/Sources/Common/Core/MatrixBuilder';
 
 export default class StackScrollTool extends BaseTool {
   touchDragCallback: Function;
@@ -40,20 +44,52 @@ export default class StackScrollTool extends BaseTool {
 
     // Stack scroll across highest resolution volume.
     const {
-      isOrthogonal,
+      orthogonalDirection,
       spacingInNormalDirection,
       imageVolume,
     } = this._getTargetVolume(scene, camera);
+
+    const volumeActor = scene.getVolumeActor(imageVolume.uid);
+
+    const scrollRange = this._getSliceRange(
+      volumeActor,
+      viewPlaneNormal,
+      focalPoint
+    );
+
+    const newFocalPoint = [...focalPoint];
+
+    debugger;
 
     // TODO calculate these bounds.
     // Cache these during a drag?
     // - Cache on mouse down?
     // - Decache on mouse up?
+    // TODO - How do we cache on mouse wheel scroll?
+    // TODO Is this tool per element or what? Could just cache here.
 
-    if (isOrthogonal) {
+    if (orthogonalDirection) {
+      this._snapFocalPointToSlice(
+        newFocalPoint,
+        scrollRange,
+        viewPlaneNormal,
+        spacingInNormalDirection
+      );
+
+      // TODO: Cache this then just increment up or down
+      // const currentSliceIndex = this._getCurrentSliceIndexOrthogonal(
+      //   imageVolume,
+      //   focalPoint,
+      //   direction
+      // );
+
       // Orthogonal, scroll through slices, capped by bounds.
+      // TODO -> Clamp current focal point to middle of slice coord.
+      // Then move by exact increments of the size.
     } else {
       // Non-orthogonal, just scroll by spacing in normal direction, capped by bounds.
+
+      console.warn('TODO! Non-orthogonal scroll');
     }
 
     const { y: deltaY } = deltaPoints.canvas;
@@ -67,6 +103,61 @@ export default class StackScrollTool extends BaseTool {
     });
     enabledElement.viewport.render();
   }
+
+  _snapFocalPointToSlice = (
+    focalPoint,
+    scrollRange,
+    viewPlaneNormal,
+    spacingInNormalDirection
+  ) => {
+    const { min, max, current } = scrollRange;
+
+    debugger;
+
+    let steps = (max - min) / spacingInNormalDirection;
+    steps = Math.floor(steps);
+
+    const slices = steps + 1; // N slices with N-1 steps between.
+
+    const fraction = (current - min) / (max - min);
+    const sliceNumber = Math.floor(fraction * steps);
+
+    let slicePos = min + sliceNumber * spacingInNormalDirection;
+
+    // Move to the center of the slice
+    slicePos += 0.5 * spacingInNormalDirection;
+
+    // Now that we have the slice in our dolly range,
+    // Dolly the focal point along the viewPlaneNormal direction
+
+    const diff = slicePos - current;
+
+    debugger;
+
+    debugger;
+  };
+
+  // private _getCurrentSliceIndexOrthogonal = (
+  //   imageVolume,
+  //   focalPoint,
+  //   direction
+  // ) => {
+  //   const { origin, spacing, dimensions } = imageVolume;
+
+  //   const { index, positive } = direction;
+
+  //   const originInDirection = origin[index];
+  //   const focalPointInDirection = focalPoint[index];
+  //   const spacingInDirection = spacing[index];
+  //   const dimensionInDirection = dimensions[index];
+  //   let slicePos = originInDirection;
+
+  //   debugger;
+
+  //   //for (let i =0; i < )
+
+  //   debugger;
+  // };
 
   private _getTargetVolume = (scene, camera) => {
     const { viewPlaneNormal } = camera;
@@ -90,13 +181,13 @@ export default class StackScrollTool extends BaseTool {
 
       const {
         spacingInNormalDirection,
-        isOrthogonal,
+        orthogonalDirection,
       } = this._getSpacingInNormalDirectionAndCheckIfOrthogonal(
         imageVolume,
         viewPlaneNormal
       );
 
-      return { imageVolume, spacingInNormalDirection, isOrthogonal };
+      return { imageVolume, spacingInNormalDirection, orthogonalDirection };
     }
 
     // Fetch volume actor with finest resolution in direction of projection.
@@ -104,7 +195,7 @@ export default class StackScrollTool extends BaseTool {
     const smallest = {
       spacingInNormalDirection: Infinity,
       imageVolume: null,
-      isOrthogonal: null,
+      orthogonalDirection: null,
     };
 
     for (let i = 0; i < numVolumeActors; i++) {
@@ -112,7 +203,7 @@ export default class StackScrollTool extends BaseTool {
 
       const {
         spacingInNormalDirection,
-        isOrthogonal,
+        orthogonalDirection,
       } = this._getSpacingInNormalDirectionAndCheckIfOrthogonal(
         imageVolume,
         viewPlaneNormal
@@ -121,11 +212,44 @@ export default class StackScrollTool extends BaseTool {
       if (spacingInNormalDirection < smallest.spacingInNormalDirection) {
         smallest.spacingInNormalDirection = spacingInNormalDirection;
         smallest.imageVolume = imageVolume;
-        smallest.isOrthogonal = isOrthogonal;
+        smallest.orthogonalDirection = orthogonalDirection;
       }
     }
 
     return smallest;
+  };
+
+  _getSliceRange = (volumeActor, viewPlaneNormal, focalPoint) => {
+    const corners = getVolumeActorCorners(volumeActor);
+
+    // Get rotation matrix from normal to +X (since bounds is aligned to XYZ)
+    const transform = vtkMatrixBuilder
+      .buildFromDegree()
+      .identity()
+      .rotateFromDirections(viewPlaneNormal, [1, 0, 0]);
+
+    corners.forEach(pt => transform.apply(pt));
+
+    let transformedFocalPoint = [...focalPoint];
+
+    transform.apply(transformedFocalPoint);
+
+    const currentSlice = transformedFocalPoint[0];
+
+    // range is now maximum X distance
+    let minX = Infinity;
+    let maxX = -Infinity;
+    for (let i = 0; i < 8; i++) {
+      const x = corners[i][0];
+      if (x > maxX) {
+        maxX = x;
+      }
+      if (x < minX) {
+        minX = x;
+      }
+    }
+
+    return { min: minX, max: maxX, current: currentSlice };
   };
 
   _getSpacingInNormalDirectionAndCheckIfOrthogonal = (
@@ -146,7 +270,7 @@ export default class StackScrollTool extends BaseTool {
       vec3.dot(kVector, viewPlaneNormal),
     ];
 
-    const isOrthogonal = this._isOrthogonal(dotProducts);
+    const orthogonalDirection = this._getOrthogonalDirection(dotProducts);
 
     const projectedSpacing = [
       dotProducts[0] * spacing[0],
@@ -156,14 +280,51 @@ export default class StackScrollTool extends BaseTool {
 
     const spacingInNormalDirection = vec3.length(projectedSpacing);
 
-    return { spacingInNormalDirection, isOrthogonal };
+    return { spacingInNormalDirection, orthogonalDirection };
   };
 
-  _isOrthogonal = dotProducts => {
-    return dotProducts.some(
-      dp => Math.abs(Math.abs(dp) - 1) < ORTHOGONAL_THRESHOLD
-    );
+  private _getOrthogonalDirection = dotProducts => {
+    let dp = dotProducts[0];
+
+    if (Math.abs(Math.abs(dp) - 1) < ORTHOGONAL_THRESHOLD) {
+      if (dp > 0) {
+        return ORTHOGONAL_DIRECTIONS.POSITIVE_I;
+      }
+
+      return ORTHOGONAL_DIRECTIONS.NEGATIVE_I;
+    }
+
+    dp = dotProducts[1];
+
+    if (Math.abs(Math.abs(dp) - 1) < ORTHOGONAL_THRESHOLD) {
+      if (dp > 0) {
+        return ORTHOGONAL_DIRECTIONS.POSITIVE_J;
+      }
+
+      return ORTHOGONAL_DIRECTIONS.NEGATIVE_J;
+    }
+
+    dp = dotProducts[2];
+
+    if (Math.abs(Math.abs(dp) - 1) < ORTHOGONAL_THRESHOLD) {
+      if (dp > 0) {
+        return ORTHOGONAL_DIRECTIONS.POSITIVE_K;
+      }
+
+      return ORTHOGONAL_DIRECTIONS.NEGATIVE_K;
+    }
+
+    return false;
   };
 }
+
+const ORTHOGONAL_DIRECTIONS = {
+  POSITIVE_I: { index: 0, positive: true },
+  POSITIVE_J: { index: 1, positive: true },
+  POSITIVE_K: { index: 2, positive: true },
+  NEGATIVE_I: { index: 0, positive: false },
+  NEGATIVE_J: { index: 1, positive: false },
+  NEGATIVE_K: { index: 2, positive: false },
+};
 
 const ORTHOGONAL_THRESHOLD = 1e-5;
