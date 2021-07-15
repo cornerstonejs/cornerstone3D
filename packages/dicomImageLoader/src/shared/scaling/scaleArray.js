821dc0364e7cb674184dcb7231a8677d9215b63f
Erik Ziegler <erik.sweed@gmail.com>
2021-07-15 13:28:22 +0200

    fix: Renaming SUV PET scaling parameter to include suvbw (SUV Body Weight) (#374)
    Co-authored-by: Davide Punzo <punzodavide@hotmail.it>

--- packages/dicomImageLoader/src/shared/scaling/scaleArray.js
+++ packages/dicomImageLoader/src/shared/scaling/scaleArray.js
@@ -2,21 +2,10 @@ export default function scaleArray(array, scalingParameters) {
   const arrayLength = array.length;
 
   if (scalingParameters.modality === 'PT') {
-    const {
-      rescaleSlope,
-      rescaleIntercept,
-      patientWeight,
-      correctedDose,
-    } = scalingParameters;
-
-    // Pre compute as much as possible
-    const patientWeightTimes1000OverCorrectedDose =
-      (patientWeight * 1000) / correctedDose;
+    const { rescaleSlope, rescaleIntercept, suvbw } = scalingParameters;
 
     for (let i = 0; i < arrayLength; i++) {
-      array[i] =
-        patientWeightTimes1000OverCorrectedDose *
-        (array[i] * rescaleSlope + rescaleIntercept);
+      array[i] = suvbw * (array[i] * rescaleSlope + rescaleIntercept);
     }
   } else {
     const { rescaleSlope, rescaleIntercept } = scalingParameters;
