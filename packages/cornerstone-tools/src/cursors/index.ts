dc1a8340858dcc8f51a1dcdb060ad252c87d93c4
James Petts <jamesapetts@gmail.com>
2021-09-08 14:41:50 +0100

    Add support for setting a cursor. (#241)
    * Add support for setting a cursor.
    
    * Remove debugger
    
    * Rename setCursor to registerCursor

--- packages/cornerstone-tools/src/cursors/index.ts
+++ packages/cornerstone-tools/src/cursors/index.ts
@@ -2,6 +2,7 @@ import MouseCursor from './MouseCursor'
 import ImageMouseCursor from './ImageMouseCursor'
 import SVGMouseCursor from './SVGMouseCursor'
 import * as elementCursor from './elementCursor'
+import { registerCursor } from './SVGCursorDescriptor'
 import type { SVGCursorDescriptor } from './SVGCursorDescriptor'
 
 export {
@@ -9,6 +10,7 @@ export {
   ImageMouseCursor,
   SVGMouseCursor,
   elementCursor,
+  registerCursor
 }
 
 export type { SVGCursorDescriptor }
