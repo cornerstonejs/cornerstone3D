dd2bfc8d085f0dda99473ba90dfc4ba2214fb2c4
Alireza <ar.sedghi@gmail.com>
2022-02-07 13:41:18 -0500

    feat: Add renderToCanvas functionality (#279)
    * feat: Add internal div that is the parent element for canvas and SVG layer
    
    * feat: Add initial react-cornerstone-viewport
    
    * fix: Tests now work with div elements instead of canvas
    
    * feat: Enable using a div instead of a canvas for viewports
    
    * feat: Renamed createCanvas and removed unnecessary usage in tests
    
    * fix: Demos now work with div element instead of canvas
    
    * feat: Add the imageLoadPool and imageRetrieval managers
    
    * feat: Add renderToCanvas functionality
    
    * feat: Add async handling to stackViewport
    
    * fix: enableElement should not fire render on RenderingEngine
    
    * fix: compile error

---
+++ packages/cornerstone-render/src/RenderingEngine/helpers/renderToCanvas.ts
@@ -0,0 +1,88 @@
+import { getRenderingEngine } from '../getRenderingEngine'
+import getOrCreateCanvas from './getOrCreateCanvas'
+import VIEWPORT_TYPE from '../../constants/viewportType'
+import ORIENTATION from '../../constants/orientation'
+import StackViewport from '../StackViewport'
+import Events from '../../enums/events'
+
+/**
+ * Renders an imageId to a Canvas Element. This method will handle creation
+ * of a tempporary enabled element, setting the imageId, and rendering the image via
+ * a StackViewport, copying the canvas drawing to the given canvas Element, and
+ * disabling the created temporary element. SuppressEvents argument is used to
+ * prevent events from firing during the render process (e.g. during a series
+ * of renders to a thumbnail image).
+ * @param {string}imageId - The imageId to render
+ * @param {HTMLCanvasElement} canvas - Canvas element to render to
+ * @param {string} renderingEngineUID - The rendering engine UID to use
+ * @param {boolean} suppressEvents - boolean to suppress events during render
+ * @returns {Promise} - A promise that resolves when the image has been rendered with the imageId
+ */
+export default function renderToCanvas(
+  imageId: string,
+  canvas: HTMLCanvasElement,
+  renderingEngineUID: string,
+  suppressEvents = true
+): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const renderingEngine = getRenderingEngine(renderingEngineUID)
+
+    if (!canvas || !(canvas instanceof HTMLCanvasElement)) {
+      throw new Error('canvas element is required')
+    }
+
+    if (!renderingEngine) {
+      throw new Error(
+        `No rendering engine with UID of ${renderingEngineUID} found`
+      )
+    }
+
+    if (renderingEngine.hasBeenDestroyed) {
+      throw new Error(
+        `Rendering engine with UID of ${renderingEngineUID} has been destroyed`
+      )
+    }
+
+    // Creating a temporary HTML element so that we can
+    // enable it and later disable it without loosing the canvas context
+    const element = document.createElement('div')
+    element.style.width = `${canvas.width}px`
+    element.style.height = `${canvas.height}px`
+
+    // Todo: we should be able to use the temporary element without appending
+    // it to the DOM
+    element.style.visibility = 'hidden'
+    document.body.appendChild(element)
+
+    // Setting the viewportUID to imageId
+    const viewportUID = imageId
+
+    const stackViewportInput = {
+      viewportUID,
+      type: VIEWPORT_TYPE.STACK,
+      element,
+      defaultOptions: {
+        orientation: ORIENTATION.AXIAL,
+        suppressEvents,
+      },
+    }
+
+    renderingEngine.enableElement(stackViewportInput)
+    const viewport = renderingEngine.getViewport(viewportUID) as StackViewport
+
+    element.addEventListener(Events.IMAGE_RENDERED, () => {
+      // get the canvas element that is the child of the div
+      const temporaryCanvas = getOrCreateCanvas(element)
+
+      // Copy the temporary canvas to the given canvas
+      const context = canvas.getContext('2d')
+
+      context.drawImage(temporaryCanvas, 0, 0)
+      renderingEngine.disableElement(viewportUID)
+      document.body.removeChild(element)
+      resolve(imageId)
+    })
+
+    viewport.setStack([imageId])
+  })
+}
