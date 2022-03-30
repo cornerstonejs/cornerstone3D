9fd971dffc01aed0df54e4a6ff53713598e8136b
Erik <erik.sweed@gmail.com>
2022-03-30 11:05:44 +0200

    re-add the dev server requirements

--- packages/docs/webpackConfigurationPlugin.js
+++ packages/docs/webpackConfigurationPlugin.js
@@ -55,3 +55,21 @@ const CopyPlugin = require('copy-webpack-plugin')
 //   },
 // };
 // };
+
+module.exports = function (context, options) {
+  return {
+    name: 'custom-docusaurus-plugin',
+    // eslint-disable-next-line
+    configureWebpack(config, isServer, utils) {
+      return {
+        devServer: {
+          historyApiFallback: true,
+          headers: {
+            'Cross-Origin-Embedder-Policy': 'require-corp',
+            'Cross-Origin-Opener-Policy': 'same-origin',
+          },
+        },
+      }
+    },
+  };
+};
