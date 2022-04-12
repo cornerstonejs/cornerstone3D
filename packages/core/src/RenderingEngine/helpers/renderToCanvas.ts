3aad51a175950771ffc70637c899cecb85ce6941
Alireza <ar.sedghi@gmail.com>
2022-04-12 11:26:50 -0400

    fix the renderToCanvas to use cpu rendering

--- packages/core/src/RenderingEngine/helpers/renderToCanvas.ts
+++ packages/core/src/RenderingEngine/helpers/renderToCanvas.ts
@@ -1,18 +1,16 @@
-import RenderingEngine from '../RenderingEngine';
-import { getRenderingEngine } from '../getRenderingEngine';
-import getOrCreateCanvas from './getOrCreateCanvas';
-import ViewportType from '../../enums/ViewportType';
-import ORIENTATION from '../../constants/orientation';
-import StackViewport from '../StackViewport';
-import Events from '../../enums/Events';
+import { IImage, CPUFallbackEnabledElement } from '../../types';
+
+import getDefaultViewport from './cpuFallback/rendering/getDefaultViewport';
+import calculateTransform from './cpuFallback/rendering/calculateTransform';
+import drawImageSync from './cpuFallback/drawImageSync';
+import { loadAndCacheImage } from '../../imageLoader';
+import * as metaData from '../../metaData';
+import { RequestType } from '../../enums';
+import imageLoadPoolManager from '../../requestPool/imageLoadPoolManager';
 
 /**
- * Renders an imageId to a Canvas. This method will handle creation
- * of a temporary enabledElement, setting the imageId, and rendering the image via
- * a StackViewport, copying the canvas drawing to the given canvas Element, and
- * disabling the created temporary element. SuppressEvents argument is used to
- * prevent events from firing during the render process (e.g. during a series
- * of renders to a thumbnail image).
+ * Renders an imageId to a Canvas. It will use the CPU rendering pipeline
+ * for image.
  *
  * @example
  * ```
@@ -23,87 +21,81 @@ import Events from '../../enums/Events';
  * ```
  * @param imageId - The imageId to render
  * @param canvas - Canvas element to render to
- * @param renderingEngineId - [Default=null] The rendering engine Id
- * to use, if not provided, will create a new rendering engine with a random Id (this is preferred)
- * @param suppressEvents - [Default = true] boolean to suppress events during render,
- * if undefined, events will be suppressed
+ * @param requestType - The type of request (default to interaction), can be 'interaction' or 'prefetch' or 'thumbnail'
+ * the order of loading for the pool manager is interaction, thumbnail, prefetch
+ * @param priority - The priority of the request within the request type (lower is higher priority)
  * @returns - A promise that resolves when the image has been rendered with the imageId
  */
 export default function renderToCanvas(
-  imageId: string,
   canvas: HTMLCanvasElement,
-  renderingEngineId = null,
-  suppressEvents = true
+  imageId: string,
+  requestType = RequestType.Interaction,
+  priority = -5
 ): Promise<string> {
   return new Promise((resolve, reject) => {
-    // If specific rendering engine is specified use that, otherwise create a
-    // new one with random uid and use that.
-    //
-    // !!!!!IMPORTANT NOTE!!!!
-    // using the same rendering engine for multiple renders
-    // is tricky since here we are listening to IMAGE_RENDERED event to copy
-    // the canvas contents to the given canvas element. This is not ideal since
-    // many things can trigger IMAGE_RENDERED including: disabling of another
-    // element (which would cause a resize event and consequently a render), or
-    // a resize event by itself (which would cause a render).
-    let renderingEngine = getRenderingEngine(renderingEngineId);
+    function successCallback(image: IImage, imageId: string) {
+      const viewport = getDefaultViewport(canvas, image);
 
-    if (!renderingEngine || renderingEngine.hasBeenDestroyed) {
-      // Use a new renderingEngine with random uid
-      renderingEngine = new RenderingEngine();
+      const enabledElement: CPUFallbackEnabledElement = {
+        canvas,
+        viewport,
+        image,
+        renderingTools: {},
+      };
+
+      enabledElement.transform = calculateTransform(enabledElement);
+
+      const invalidated = true;
+      drawImageSync(enabledElement, invalidated);
+
+      resolve(imageId);
     }
 
-    if (!canvas || !(canvas instanceof HTMLCanvasElement)) {
-      throw new Error('canvas element is required');
+    function errorCallback(error: Error, imageId: string) {
+      console.error(error, imageId);
+      reject(error);
     }
 
-    if (!renderingEngine) {
-      throw new Error(
-        `No rendering engine with Id of ${renderingEngineId} found`
+    function sendRequest(imageId, imageIdIndex, options) {
+      return loadAndCacheImage(imageId, options).then(
+        (image) => {
+          successCallback.call(this, image, imageIdIndex, imageId);
+        },
+        (error) => {
+          errorCallback.call(this, error, imageIdIndex, imageId);
+        }
       );
     }
 
-    // Creating a temporary HTML element so that we can
-    // enable it and later disable it without loosing the canvas context
-    const element = document.createElement('div');
-    element.style.width = `${canvas.width}px`;
-    element.style.height = `${canvas.height}px`;
+    const modalityLutModule = metaData.get('modalityLutModule', imageId) || {};
+    const suvFactor = metaData.get('scalingModule', imageId) || {};
 
-    // Todo: we should be able to use the temporary element without appending
-    // it to the DOM
-    element.style.visibility = 'hidden';
-    document.body.appendChild(element);
+    const generalSeriesModule =
+      metaData.get('generalSeriesModule', imageId) || {};
 
-    // Setting the viewportId to imageId
-    const viewportId = imageId;
+    const scalingParameters = {
+      rescaleSlope: modalityLutModule.rescaleSlope,
+      rescaleIntercept: modalityLutModule.rescaleIntercept,
+      modality: generalSeriesModule.modality,
+      suvbw: suvFactor.suvbw,
+    };
 
-    const stackViewportInput = {
-      viewportId,
-      type: ViewportType.STACK,
-      element,
-      defaultOptions: {
-        orientation: ORIENTATION.AXIAL,
-        suppressEvents,
+    const options = {
+      targetBuffer: {
+        type: 'Float32Array',
+        offset: null,
+        length: null,
+      },
+      preScale: {
+        scalingParameters,
       },
     };
 
-    renderingEngine.enableElement(stackViewportInput);
-    const viewport = renderingEngine.getViewport(viewportId) as StackViewport;
-
-    element.addEventListener(Events.IMAGE_RENDERED, () => {
-      // get the canvas element that is the child of the div
-      const temporaryCanvas = getOrCreateCanvas(element);
-
-      // Copy the temporary canvas to the given canvas
-      const context = canvas.getContext('2d');
-
-      context.drawImage(temporaryCanvas, 0, 0);
-      renderingEngine.disableElement(viewportId);
-      document.body.removeChild(element);
-      renderingEngine.destroy();
-      resolve(imageId);
-    });
-
-    viewport.setStack([imageId]);
+    imageLoadPoolManager.addRequest(
+      sendRequest.bind(null, imageId, options),
+      requestType,
+      { imageId },
+      priority
+    );
   });
 }
