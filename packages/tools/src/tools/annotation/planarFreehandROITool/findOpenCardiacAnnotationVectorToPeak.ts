a93425c3c60db782347587d60597192cd1f0c039
James A. Petts <jamesapetts@gmail.com>
2022-05-12 10:50:21 +0100

    Update line finding to find longest segment

--- packages/tools/src/tools/annotation/planarFreehandROITool/findOpenCardiacAnnotationVectorToPeak.ts
+++ packages/tools/src/tools/annotation/planarFreehandROITool/findOpenCardiacAnnotationVectorToPeak.ts
@@ -1,12 +1,9 @@
 import type { Types } from '@cornerstonejs/core';
 import { PlanarFreehandROIAnnotation } from '../../../types/ToolSpecificAnnotationTypes';
-import { polyline } from '../../../utilities/math';
-
-const { getClosestIntersectionWithPolyline } = polyline;
 import { vec2 } from 'gl-matrix';
 
 /**
- * Finds the length of the perpendicular line from the midpoint of the line
+ * Finds the length of the longest line from the midpoint of the line
  * that joins the start and end of the open contour, to the surface of the
  * open contour.
  */
@@ -39,102 +36,28 @@ export default function findOpenCardiacAnnotationVectorToPeak(
     (first[1] + last[1]) / 2,
   ];
 
-  // Find the size of the annotation so we have a good line to check the crossing on.
-  let xMin = Infinity;
-  let xMax = -Infinity;
-  let yMin = Infinity;
-  let yMax = -Infinity;
+  // Get furthest point.
+
+  const furthest = {
+    dist: 0,
+    index: null,
+  };
 
   for (let i = 0; i < canvasPoints.length; i++) {
-    const [x, y] = canvasPoints[i];
+    const canvasPoint = canvasPoints[i];
 
-    if (x > xMax) {
-      xMax = x;
-    } else if (x < xMin) {
-      xMin = x;
-    }
+    const distance = vec2.dist(canvasPoint, <vec2>centerOfFirstToLast);
 
-    if (y > yMax) {
-      yMax = y;
-    } else if (y < yMin) {
-      yMin = y;
+    if (distance > furthest.dist) {
+      furthest.dist = distance;
+      furthest.index = i;
     }
   }
 
-  const height = yMax - yMin;
-  const width = xMax - xMin;
+  const toFurthest = [canvasPoints[furthest.index], centerOfFirstToLast];
+  const toFurthestWorld = toFurthest.map(viewport.canvasToWorld);
 
-  const diagonal = Math.sqrt(width * width + height * height);
-
-  // Make the line a bit longer to make sure it crosses in the most extreme case.
-  const testCrossLength = diagonal * 1.1;
-
-  const testChord1: Types.Point2[] = [
-    [centerOfFirstToLast[0], centerOfFirstToLast[1]],
-    [
-      centerOfFirstToLast[0] + normalVector1[0] * testCrossLength,
-      centerOfFirstToLast[1] + normalVector1[1] * testCrossLength,
-    ],
-  ];
-
-  const testChord2: Types.Point2[] = [
-    [centerOfFirstToLast[0], centerOfFirstToLast[1]],
-    [
-      centerOfFirstToLast[0] + normalVector2[0] * testCrossLength,
-      centerOfFirstToLast[1] + normalVector2[1] * testCrossLength,
-    ],
-  ];
-
-  const testChord1CrossingData = getClosestIntersectionWithPolyline(
-    canvasPoints,
-    testChord1[0],
-    testChord1[1],
-    false
-  );
-  const testChord2CrossingData = getClosestIntersectionWithPolyline(
-    canvasPoints,
-    testChord2[0],
-    testChord2[1],
-    false
-  );
-
-  // Find which normal direction to use and the distance of the chord
-  let normalToUse;
-  let distance;
-
-  if (
-    testChord1CrossingData !== undefined &&
-    testChord2CrossingData !== undefined
-  ) {
-    // Find the furthest crossing if both cross.
-    if (testChord1CrossingData.distance > testChord2CrossingData.distance) {
-      normalToUse = normalVector1;
-      distance = testChord1CrossingData.distance;
-    } else {
-      normalToUse = normalVector2;
-      distance = testChord1CrossingData.distance;
-    }
-  } else if (testChord1CrossingData === undefined) {
-    normalToUse = normalVector2;
-    distance = testChord2CrossingData.distance;
-  } else {
-    normalToUse = normalVector1;
-    distance = testChord1CrossingData.distance;
-  }
-
-  const chordCanvas = [
-    [centerOfFirstToLast[0], centerOfFirstToLast[1]],
-    [
-      centerOfFirstToLast[0] + normalToUse[0] * distance,
-      centerOfFirstToLast[1] + normalToUse[1] * distance,
-    ],
-  ];
-
-  // Convert that line to world coordinates and return
-  // return the world coordinates
-  const chordWorldPos = chordCanvas.map(viewport.canvasToWorld);
-
-  return chordWorldPos;
+  return toFurthestWorld;
 }
 
 export function findOpenCardiacAnnotationVectorToPeakOnRender(
