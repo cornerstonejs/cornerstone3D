b134b2c7d42c147028bc5d8b9f683c5ca7497cce
James Manners <james@binary.com.au>
2023-01-04 14:16:43 +1100

    [dicom-loader] update CornerstoneWadoLoaderOptions to include optional params

--- packages/dicom-image-loader/src/imageLoader/internal/options.ts
+++ packages/dicom-image-loader/src/imageLoader/internal/options.ts
@@ -5,24 +5,24 @@ import {
 
 export interface CornerstoneWadoLoaderOptions {
   // callback allowing customization of the xhr (e.g. adding custom auth headers, cors, etc)
-  beforeSend: (
+  beforeSend?: (
     xhr: XMLHttpRequest,
     imageId: string,
     defaultHeaders: Record<string, string>,
     params: CornerstoneWadoLoaderXhrRequestParams
   ) => Record<string, string> | void;
   // callback allowing modification of the xhr response before creating image objects
-  beforeProcessing: (xhr: XMLHttpRequest) => Promise<ArrayBuffer>;
+  beforeProcessing?: (xhr: XMLHttpRequest) => Promise<ArrayBuffer>;
   // callback allowing modification of newly created image objects
-  imageCreated: (...args: any[]) => void;
+  imageCreated?: (...args: any[]) => void;
   onloadstart?: (event: ProgressEvent<EventTarget>, params: any) => void;
   onloadend?: (event: ProgressEvent<EventTarget>, params: any) => void;
   onreadystatechange?: (event: Event, params: any) => void;
   onprogress?: (event: ProgressEvent<EventTarget>, params: any) => void;
   errorInterceptor?: (error: CornerstoneWadoLoaderXhrRequestError) => void;
-  strict: boolean;
-  decodeConfig: {
-    convertFloatPixelDataToInt: boolean;
+  strict?: boolean;
+  decodeConfig?: {
+    convertFloatPixelDataToInt?: boolean;
   };
 }
 
