22ed31fdaeabadc81a73b2878793651965a4d3ab
Alireza <ar.sedghi@gmail.com>
2023-01-16 15:53:45 -0500

    update cswil

--- utils/demo/helpers/initCornerstoneWADOImageLoader.js
+++ utils/demo/helpers/initCornerstoneWADOImageLoader.js
@@ -14,6 +14,7 @@ export default function initCornerstoneWADOImageLoader() {
     useWebWorkers: true,
     decodeConfig: {
       convertFloatPixelDataToInt: false,
+      use16BitDataType: true,
     },
   });
 
