fdbc3d600f75b9a4a25342c7ff0af4e93fecd63f
Joe Boccanfuso <joe.boccanfuso@radicalimaging.com>
2023-01-18 12:52:28 -0500

    fix(StackViewport): #372 Reset camera bug when rotation happens on StackViewport
    For a GPU reset, start by resetting the flips and rotation (via viewUp) because
    the ordering of the various flips and rotations that have been applied is not known.

--- packages/core/src/RenderingEngine/StackViewport.ts
+++ packages/core/src/RenderingEngine/StackViewport.ts
@@ -1880,10 +1880,12 @@ class StackViewport extends Viewport implements IStackViewport {
   }
 
   private resetCameraGPU(resetPan, resetZoom): boolean {
-    // Todo: we need to make the rotation a camera properties so that
-    // we can reset it there, right now it is not possible to reset the rotation
-    // without this
-    this.getVtkActiveCamera().roll(this.rotationCache);
+    // We do not know the ordering of various flips and rotations that have been applied, so just start like we were at the beginning.
+    this.setCamera({
+      flipHorizontal: false,
+      flipVertical: false,
+      viewUp: <Point3>[0, -1, 0],
+    });
 
     // For stack Viewport we since we have only one slice
     // it should be enough to reset the camera to the center of the image
