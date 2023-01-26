733d81bc951e09f6063ce423a5e463a648080e99
Diego Hennrich <diegohennrich@gmail.com>
2023-01-26 10:32:46 -0300

    Listeners Reorganized

--- packages/tools/src/tools/segmentation/SphereScissorsTool.ts
+++ packages/tools/src/tools/segmentation/SphereScissorsTool.ts
@@ -274,8 +274,9 @@ class SphereScissorsTool extends BaseTool {
     element.addEventListener(Events.MOUSE_UP, this._endCallback);
     element.addEventListener(Events.MOUSE_DRAG, this._dragCallback);
     element.addEventListener(Events.MOUSE_CLICK, this._endCallback);
-    element.addEventListener(Events.TOUCH_TAP, this._endCallback);
+
     element.addEventListener(Events.TOUCH_END, this._endCallback);
+    element.addEventListener(Events.TOUCH_TAP, this._endCallback);
     element.addEventListener(Events.TOUCH_DRAG, this._dragCallback);
   };
 
@@ -286,9 +287,10 @@ class SphereScissorsTool extends BaseTool {
     element.removeEventListener(Events.MOUSE_UP, this._endCallback);
     element.removeEventListener(Events.MOUSE_DRAG, this._dragCallback);
     element.removeEventListener(Events.MOUSE_CLICK, this._endCallback);
-    element.removeEventListener(Events.TOUCH_TAP, this._endCallback);
+
     element.removeEventListener(Events.TOUCH_END, this._endCallback);
     element.removeEventListener(Events.TOUCH_DRAG, this._dragCallback);
+    element.removeEventListener(Events.TOUCH_TAP, this._endCallback);
   };
 
   /**
