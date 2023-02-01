fc786eb98e85a5d955ac5f82e5adde13257bff31
Bill Wallace <wayfarer3130@gmail.com>
2023-02-01 07:18:23 -0500

    fix: Update rollup to much newer version
    Update a dependency
    
    fix: Compatibility with building in older node environments
    
    fix: Remove src exports

---
+++ packages/adapters/rollup.config.mjs
@@ -0,0 +1,39 @@
+import babel from "@rollup/plugin-babel";
+import typescript from "@rollup/plugin-typescript";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import { readFileSync } from "fs";
+
+const pkg = JSON.parse(readFileSync('package.json', {encoding: 'utf8'}));
+
+
+export default {
+  input: pkg.src || 'src/index.ts',
+  output: [
+    // {
+    //   file: `dist/${pkg.name}.js`,
+    //   format: "umd",
+    //   name: pkg.name,
+    //   sourcemap: true
+    // },
+    {
+      file: `dist/${pkg.name}.es.js`,
+      format: "es",
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    resolve({
+      browser: true
+    }),
+    commonjs(),
+    typescript(),
+//    globals(),
+    // builtins(),
+    babel({
+      exclude: "node_modules/**"
+    }),
+    json()
+  ]
+};
