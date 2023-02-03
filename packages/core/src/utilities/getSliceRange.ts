7138372b185b5d7c2cc76a8e2b257a94e1751f42
Bill Wallace <wayfarer3130@gmail.com>
2023-02-03 15:47:42 -0500

    fix(SliceRange): slice range for oblique image (#408)
    * fix:Acquisition range is computed incorrectly sometimes
    
    * PR changes - use the new logic only for non-IJK vectors
    
    * fix: Acquisition range was wrong, updated comments
    
    * PR comments

--- packages/core/src/utilities/getSliceRange.ts
+++ packages/core/src/utilities/getSliceRange.ts
@@ -2,6 +2,12 @@ import vtkMatrixBuilder from '@kitware/vtk.js/Common/Core/MatrixBuilder';
 import getVolumeActorCorners from './getVolumeActorCorners';
 import type { VolumeActor, Point3, ActorSliceRange } from '../types';
 
+const isOne = (v) => Math.abs(v) > 0.99 && Math.abs(v) < 1.01;
+const isUnit = (v, off) =>
+  isOne(v[off]) || isOne(v[off + 1]) || isOne(v[off + 2]);
+
+const isOrthonormal = (v) => isUnit(v, 0) && isUnit(v, 3) && isUnit(v, 6);
+
 /**
  * Given a `vtkVolumeActor`, and a normal direction,
  * calculate the range of slices in the focal normal direction that encapsulate
@@ -19,8 +25,28 @@ export default function getSliceRange(
   viewPlaneNormal: Point3,
   focalPoint: Point3
 ): ActorSliceRange {
-  const corners = getVolumeActorCorners(volumeActor);
+  const imageData = volumeActor.getMapper().getInputData();
+  let corners;
+  const direction = imageData.getDirection();
 
+  if (isOrthonormal(direction)) {
+    // This logic is only valid when the IJK vectors are unit vectors
+    corners = getVolumeActorCorners(volumeActor);
+  } else {
+    // This logic works for both unit and non-unit vectors, but is slower
+    const [dx, dy, dz] = imageData.getDimensions();
+    const cornersIdx = [
+      [0, 0, 0],
+      [dx - 1, 0, 0],
+      [0, dy - 1, 0],
+      [dx - 1, dy - 1, 0],
+      [0, 0, dz - 1],
+      [dx - 1, 0, dz - 1],
+      [0, dy - 1, dz - 1],
+      [dx - 1, dy - 1, dz - 1],
+    ];
+    corners = cornersIdx.map((it) => imageData.indexToWorld(it));
+  }
   // Get rotation matrix from normal to +X (since bounds is aligned to XYZ)
   const transform = vtkMatrixBuilder
     .buildFromDegree()
@@ -30,7 +56,6 @@ export default function getSliceRange(
   corners.forEach((pt) => transform.apply(pt));
 
   const transformedFocalPoint = [...focalPoint];
-
   transform.apply(transformedFocalPoint);
 
   const currentSlice = transformedFocalPoint[0];
