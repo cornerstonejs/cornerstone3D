bff23ecfe551312a1339211ddb7838993514a377
Bill Wallace <wayfarer3130@gmail.com>
2023-02-16 14:40:05 -0500

    fix(adapter): The rectangle encoding of SR (#437)
    * fix: The rectangle renders as a Z, render it as a rectangle
    
    * fix: Save correct header on measurements

--- packages/adapters/src/adapters/Cornerstone3D/MeasurementReport.ts
+++ packages/adapters/src/adapters/Cornerstone3D/MeasurementReport.ts
@@ -252,7 +252,7 @@ export default class MeasurementReport {
                 "sopCommonModule",
                 imageId
             );
-            const instance = metadataProvider.getInstance(imageId);
+            const instance = metadataProvider.get("instance", imageId);
 
             const { sopInstanceUID, sopClassUID } = sopCommonModule;
             const { SeriesInstanceUID: seriesInstanceUID } = instance;
