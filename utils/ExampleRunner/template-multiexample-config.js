d00ab6c5875bb1fdc20009dfea423eacf35727f6
Alireza <ar.sedghi@gmail.com>
2023-03-07 07:51:58 -0500

    wip

--- utils/ExampleRunner/template-multiexample-config.js
+++ utils/ExampleRunner/template-multiexample-config.js
@@ -9,7 +9,7 @@ const csStreamingBasePath = path.resolve(
   './packages/streaming-image-volume-loader/src/index'
 );
 const csDICOMImageLoaderDistPath = path.resolve(
-  'packages/dicomImageLoader/dist/dynamic-import/cornerstoneDICOMImageLoader.min.js'
+  './packages/dicomImageLoader/dist/dynamic-import/cornerstoneDICOMImageLoader.min.js'
 );
 
 module.exports = function buildConfig(names, exampleBasePaths, destPath, root) {
