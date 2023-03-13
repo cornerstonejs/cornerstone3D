024590b08c3eaf60aa500a5ccf6aadbfac44036a
Ouwen Huang <ouwen.huang@duke.edu>
2023-03-13 02:13:09 +0000

    feat: added out of bounds scroll

--- packages/core/src/enums/Events.ts
+++ packages/core/src/enums/Events.ts
@@ -188,6 +188,11 @@ enum Events {
    */
   GEOMETRY_CACHE_GEOMETRY_ADDED = 'CORNERSTONE_GEOMETRY_CACHE_GEOMETRY_ADDED',
 
+  /**
+   * Triggers when the scroll function is called with a delta that is out of bounds.
+   * This is usually for signaling that the user may want a different volume for partially loaded volumes which is meant to optimize memory.
+   */
+  VOLUME_SCROLL_OUT_OF_BOUNDS = 'CORNERSTONE_VOLUME_SCROLL_OUT_OF_BOUNDS',
   // IMAGE_CACHE_FULL = 'CORNERSTONE_IMAGE_CACHE_FULL',
   // PRE_RENDER = 'CORNERSTONE_PRE_RENDER',
   // ELEMENT_RESIZED = 'CORNERSTONE_ELEMENT_RESIZED',
