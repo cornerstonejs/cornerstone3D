2a8ea9ad862ca349399fc3532274a39a99a36234
Alireza <ar.sedghi@gmail.com>
2023-05-22 13:56:19 -0400

    another commit

--- packages/core/src/RenderingEngine/RenderingEngine.ts
+++ packages/core/src/RenderingEngine/RenderingEngine.ts
@@ -150,7 +150,7 @@ class RenderingEngine implements IRenderingEngine {
     if (viewport) {
       console.log('Viewport already exists, disabling it first');
       this.disableElement(viewportId);
-      console.log('Viewport disabled');
+      console.log(`Viewport ${viewportId} disabled`);
     }
 
     // 2.a) See if viewport uses a custom rendering pipeline.
