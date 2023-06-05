8eaf9b797257d91e20669b2c46333c6b6680a00c
Bill Wallace <wayfarer3130@gmail.com>
2023-06-05 13:04:16 -0400

    fix(synchronizer): The synchronizer needs to discontinue listening until done

--- packages/tools/src/store/SynchronizerManager/Synchronizer.ts
+++ packages/tools/src/store/SynchronizerManager/Synchronizer.ts
@@ -202,6 +202,7 @@ class Synchronizer {
     }
 
     this._ignoreFiredEvents = true;
+    const promises = [];
     try {
       for (let i = 0; i < this._targetViewports.length; i++) {
         const targetViewport = this._targetViewports[i];
@@ -211,13 +212,20 @@ class Synchronizer {
         if (targetIsSource) {
           continue;
         }
-
-        this._eventHandler(this, sourceViewport, targetViewport, sourceEvent);
+        promises.push(
+          this._eventHandler(this, sourceViewport, targetViewport, sourceEvent)
+        );
       }
     } catch (ex) {
       console.warn(`Synchronizer, for: ${this._eventName}`, ex);
     } finally {
-      this._ignoreFiredEvents = false;
+      if (promises.length) {
+        Promise.allSettled(promises).then(() => {
+          this._ignoreFiredEvents = false;
+        });
+      } else {
+        this._ignoreFiredEvents = false;
+      }
     }
   }
 
