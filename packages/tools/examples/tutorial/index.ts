908dfca7edbd3558080afcbc4b57de6c2073544d
Sofien-Sellami <sofien.sellami@esprit.tn>
2023-06-13 16:56:44 +0200

    review updates

---
+++ packages/tools/examples/tutorial/index.ts
@@ -0,0 +1,73 @@
+/**
+ * WARNING
+ * DO NOT REMOVE ANY OF THE BELOW IMPORT STATEMENTS
+ * SOME ARE USED FOR SOME OF THE TUTORIALS, AND WILL BREAK IF REMOVED
+ */
+
+import {
+  RenderingEngine,
+  Types,
+  Enums,
+  setVolumesForViewports,
+  volumeLoader,
+} from '@cornerstonejs/core';
+import {
+  addTool,
+  BrushTool,
+  SegmentationDisplayTool,
+  BidirectionalTool,
+  ToolGroupManager,
+  WindowLevelTool,
+  ZoomTool,
+  segmentation,
+  Enums as csToolsEnums,
+} from '@cornerstonejs/tools';
+import {
+  initDemo,
+  createImageIdsAndCacheMetaData,
+  setTitleAndDescription,
+} from '../../../../utils/demo/helpers';
+
+// This is for debugging purposes
+console.warn(
+  'Click on index.ts to open source code for this example --------->'
+);
+
+// ============================= //
+// ======== Set up page ======== //
+setTitleAndDescription(
+  'Tutorial Playground',
+  'The playground for you to copy paste the codes in the tutorials and run it'
+);
+
+const { ViewportType } = Enums;
+/**
+ * Runs the demo
+ */
+async function run() {
+  // Init Cornerstone and related libraries
+  await initDemo();
+
+  /**
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   * Copy-paste the code from tutorials below to try them locally.
+   * You can run the tutorial after by running `yarn run example tutorial` when
+   * you are at the root of the tools package directory.
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   */
+}
+
+run();
