c471b3913c9562b0f707b7a36354c9b014643bf2
Alireza <ar.sedghi@gmail.com>
2023-07-25 15:17:19 -0400

    revert example change

--- packages/tools/examples/stackToVolumeWithAnnotations/index.ts
+++ packages/tools/examples/stackToVolumeWithAnnotations/index.ts
@@ -2,18 +2,20 @@ import {
   RenderingEngine,
   Types,
   Enums,
-  setVolumesForViewports,
-  volumeLoader,
   getRenderingEngine,
 } from '@cornerstonejs/core';
 import {
   initDemo,
   createImageIdsAndCacheMetaData,
   setTitleAndDescription,
-  setCtTransferFunctionForVolumeActor,
   addDropdownToToolbar,
+  addButtonToToolbar,
 } from '../../../../utils/demo/helpers';
 import * as cornerstoneTools from '@cornerstonejs/tools';
+import {
+  _convertVolumeToStackViewport,
+  _convertStackToVolumeViewport,
+} from './_setViewports';
 
 // This is for debugging purposes
 console.warn(
@@ -21,181 +23,208 @@ console.warn(
 );
 
 const {
+  LengthTool,
+  ProbeTool,
+  RectangleROITool,
+  EllipticalROITool,
+  CircleROITool,
+  BidirectionalTool,
+  AngleTool,
+  CobbAngleTool,
   ToolGroupManager,
-  Enums: csToolsEnums,
-  CrosshairsTool,
+  ArrowAnnotateTool,
   StackScrollMouseWheelTool,
-  ZoomTool,
+  Enums: csToolsEnums,
 } = cornerstoneTools;
 
-const { MouseBindings } = csToolsEnums;
 const { ViewportType } = Enums;
-
-// Define a unique id for the volume
-const volumeName = 'CT_VOLUME_ID'; // Id of the volume less loader prefix
-const volumeLoaderScheme = 'cornerstoneStreamingImageVolume'; // Loader id which defines which volume loader to use
-const volumeId = `${volumeLoaderScheme}:${volumeName}`; // VolumeId with loader id + volume id
-const toolGroupId = 'MY_TOOLGROUP_ID';
+const { MouseBindings } = csToolsEnums;
 
 // ======== Set up page ======== //
 setTitleAndDescription(
-  'Crosshairs',
-  'Here we demonstrate crosshairs linking three orthogonal views of the same data. You can select the blend mode that will be used if you modify the slab thickness of the crosshairs by dragging the control points.'
+  'Stack and VolumeViewport conversions',
+  'In this demo, you see how the stack and volume viewport conversions work. The purple background represents a StackViewport while the green background represents a VolumeViewport. You can start annotating the images and annotations will be rendered correctly regardless of the viewport they were drawn on.'
 );
 
-const size = '500px';
 const content = document.getElementById('content');
-const reloadButton = document.createElement('button');
-reloadButton.innerHTML = 'Reload Image';
-reloadButton.addEventListener('click', () => {
-  run(
-    Math.random(),
-    '1.3.6.1.4.1.14519.5.2.1.7009.2403.334240657131972136850343327463',
-    '1.3.6.1.4.1.14519.5.2.1.7009.2403.226151125820845824875394858561',
-    'https://d3t6nz73ql33tx.cloudfront.net/dicomweb'
-  );
-});
-const viewportGrid = document.createElement('div');
-
-viewportGrid.style.display = 'flex';
-viewportGrid.style.display = 'flex';
-viewportGrid.style.flexDirection = 'row';
-
-const element1 = document.createElement('div');
-const element2 = document.createElement('div');
-const element3 = document.createElement('div');
-element1.style.width = size;
-element1.style.height = size;
-element2.style.width = size;
-element2.style.height = size;
-element3.style.width = size;
-element3.style.height = size;
+const element = document.createElement('div');
 
 // Disable right click context menu so we can have right click tools
-element1.oncontextmenu = (e) => e.preventDefault();
-element2.oncontextmenu = (e) => e.preventDefault();
-element3.oncontextmenu = (e) => e.preventDefault();
+element.oncontextmenu = (e) => e.preventDefault();
 
-viewportGrid.appendChild(element1);
-viewportGrid.appendChild(element2);
-viewportGrid.appendChild(element3);
+element.id = 'cornerstone-element';
+element.style.width = '500px';
+element.style.height = '500px';
 
-content.appendChild(viewportGrid);
-content.appendChild(reloadButton);
+content.appendChild(element);
 
 const instructions = document.createElement('p');
-instructions.innerText = `
-  Basic controls:
-  - Click/Drag anywhere in the viewport to move the center of the crosshairs.
-  - Drag a reference line to move it, scrolling the other views.
-
-  Advanced controls: Hover over a line and find the following two handles:
-  - Square (closest to center): Drag these to change the thickness of the MIP slab in that plane.
-  - Circle (further from center): Drag these to rotate the axes.
-  `;
+instructions.innerText = 'Left Click to use selected tool';
 
 content.append(instructions);
-
 // ============================= //
 
-const viewportId1 = 'CT_AXIAL';
-const viewportId2 = 'CT_SAGITTAL';
-const viewportId3 = 'CT_CORONAL';
+const toolGroupId = 'STACK_TOOL_GROUP_ID';
+
+const toolsNames = [
+  LengthTool.toolName,
+  ProbeTool.toolName,
+  RectangleROITool.toolName,
+  EllipticalROITool.toolName,
+  CircleROITool.toolName,
+  BidirectionalTool.toolName,
+  AngleTool.toolName,
+  CobbAngleTool.toolName,
+  ArrowAnnotateTool.toolName,
+];
+let selectedToolName = toolsNames[0];
+const renderingEngineId = 'myRenderingEngine';
+const viewportId = 'CT_VIEWPORT';
+let toolGroup;
+
+addDropdownToToolbar({
+  options: { values: toolsNames, defaultValue: selectedToolName },
+  onSelectedValueChange: (newSelectedToolNameAsStringOrNumber) => {
+    const newSelectedToolName = String(newSelectedToolNameAsStringOrNumber);
+
+    // Set the new tool active
+    toolGroup.setToolActive(newSelectedToolName, {
+      bindings: [
+        {
+          mouseButton: MouseBindings.Primary, // Left Click
+        },
+      ],
+    });
+
+    // Set the old tool passive
+    toolGroup.setToolPassive(selectedToolName);
+
+    selectedToolName = <string>newSelectedToolName;
+  },
+});
+
+addButtonToToolbar({
+  title: 'Switch StackViewport to VolumeViewport, and vice versa',
+  onClick: () => {
+    // Get the rendering engine
+    const renderingEngine = getRenderingEngine(renderingEngineId);
+
+    const viewport = renderingEngine.getViewport(viewportId);
+
+    if (viewport.type === ViewportType.STACK) {
+      _convertStackToVolumeViewport(
+        renderingEngine,
+        viewport as Types.IStackViewport,
+        toolGroup
+      );
+      return;
+    }
+
+    // convert to stack
+    _convertVolumeToStackViewport(
+      renderingEngine,
+      viewport as Types.IVolumeViewport,
+      toolGroup
+    );
+  },
+});
 
 /**
  * Runs the demo
  */
-async function run(
-  volumeName,
-  StudyInstanceUID,
-  SeriesInstanceUID,
-  wadoRsRoot
-) {
-  // Define a unique id for the volume
-  const volumeLoaderScheme = 'cornerstoneStreamingImageVolume'; // Loader id which defines which volume loader to use
-  const volumeId = `${volumeLoaderScheme}:${volumeName}`; // VolumeId with loader id + volume id
-  const toolGroupId = 'MY_TOOLGROUP_ID';
+async function run() {
   // Init Cornerstone and related libraries
   await initDemo();
 
-  // Get Cornerstone imageIds for the source data and fetch metadata into RAM
-  const imageIds = await createImageIdsAndCacheMetaData({
-    StudyInstanceUID,
-    SeriesInstanceUID,
-    wadoRsRoot,
-  });
+  // Add tools to Cornerstone3D
+  cornerstoneTools.addTool(LengthTool);
+  cornerstoneTools.addTool(ProbeTool);
+  cornerstoneTools.addTool(RectangleROITool);
+  cornerstoneTools.addTool(EllipticalROITool);
+  cornerstoneTools.addTool(CircleROITool);
+  cornerstoneTools.addTool(BidirectionalTool);
+  cornerstoneTools.addTool(AngleTool);
+  cornerstoneTools.addTool(CobbAngleTool);
+  cornerstoneTools.addTool(ArrowAnnotateTool);
+  cornerstoneTools.addTool(StackScrollMouseWheelTool);
 
-  // Define a volume in memory
-  const volume = await volumeLoader.createAndCacheVolume(volumeId, {
-    imageIds,
-  });
-  const renderingEngineId = 'myRenderingEngine';
-  let renderingEngine;
-  // Instantiate a rendering engine
-  if (!getRenderingEngine(renderingEngineId)) {
-    renderingEngine = new RenderingEngine(renderingEngineId);
-  } else {
-    renderingEngine = getRenderingEngine(renderingEngineId);
-  }
+  // Define a tool group, which defines how mouse events map to tool commands for
+  // Any viewport using the group
+  toolGroup = ToolGroupManager.createToolGroup(toolGroupId);
 
-  // Create the viewports
-  const viewportInputArray = [
-    {
-      viewportId: viewportId1,
-      type: ViewportType.ORTHOGRAPHIC,
-      element: element1,
-      defaultOptions: {
-        orientation: Enums.OrientationAxis.AXIAL,
-        background: <Types.Point3>[0, 0, 0],
-      },
-    },
-    {
-      viewportId: viewportId2,
-      type: ViewportType.ORTHOGRAPHIC,
-      element: element2,
-      defaultOptions: {
-        orientation: Enums.OrientationAxis.SAGITTAL,
-        background: <Types.Point3>[0, 0, 0],
-      },
-    },
-    {
-      viewportId: viewportId3,
-      type: ViewportType.ORTHOGRAPHIC,
-      element: element3,
-      defaultOptions: {
-        orientation: Enums.OrientationAxis.CORONAL,
-        background: <Types.Point3>[0, 0, 0],
-      },
-    },
-  ];
+  // Add the tools to the tool group
+  toolGroup.addTool(LengthTool.toolName);
+  toolGroup.addTool(ProbeTool.toolName);
+  toolGroup.addTool(RectangleROITool.toolName);
+  toolGroup.addTool(EllipticalROITool.toolName);
+  toolGroup.addTool(CircleROITool.toolName);
+  toolGroup.addTool(BidirectionalTool.toolName);
+  toolGroup.addTool(AngleTool.toolName);
+  toolGroup.addTool(CobbAngleTool.toolName);
+  toolGroup.addTool(ArrowAnnotateTool.toolName);
+  toolGroup.addTool(StackScrollMouseWheelTool.toolName);
 
-  if (!renderingEngine.getViewport('CT_CORONAL')) {
-    renderingEngine.setViewports(viewportInputArray);
-  }
-  // Set the volume to load
-  await volume.load();
-
-  // Set volumes on the viewports
-  await setVolumesForViewports(
-    renderingEngine,
-    [
+  // Set the initial state of the tools, here we set one tool active on left click.
+  // This means left click will draw that tool.
+  toolGroup.setToolActive(LengthTool.toolName, {
+    bindings: [
       {
-        volumeId,
-        callback: setCtTransferFunctionForVolumeActor,
+        mouseButton: MouseBindings.Primary, // Left Click
       },
     ],
-    [viewportId1, viewportId2, viewportId3]
+  });
+  // We set all the other tools passive here, this means that any state is rendered, and editable
+  // But aren't actively being drawn (see the toolModes example for information)
+  toolGroup.setToolActive(StackScrollMouseWheelTool.toolName);
+  toolGroup.setToolPassive(ProbeTool.toolName);
+  toolGroup.setToolPassive(RectangleROITool.toolName);
+  toolGroup.setToolPassive(EllipticalROITool.toolName);
+  toolGroup.setToolPassive(CircleROITool.toolName);
+  toolGroup.setToolPassive(BidirectionalTool.toolName);
+  toolGroup.setToolPassive(AngleTool.toolName);
+  toolGroup.setToolPassive(CobbAngleTool.toolName);
+  toolGroup.setToolPassive(ArrowAnnotateTool.toolName);
+
+  // Get Cornerstone imageIds and fetch metadata into RAM
+  const imageIds = await createImageIdsAndCacheMetaData({
+    StudyInstanceUID:
+      '1.3.6.1.4.1.14519.5.2.1.7009.2403.334240657131972136850343327463',
+    SeriesInstanceUID:
+      '1.3.6.1.4.1.14519.5.2.1.7009.2403.226151125820845824875394858561',
+    wadoRsRoot: 'https://d3t6nz73ql33tx.cloudfront.net/dicomweb',
+  });
+
+  // Instantiate a rendering engine
+  const renderingEngine = new RenderingEngine(renderingEngineId);
+
+  // Create a stack viewport
+  const viewportInput = {
+    viewportId,
+    type: ViewportType.STACK,
+    element,
+    defaultOptions: {
+      background: <Types.Point3>[0.4, 0, 0.4],
+    },
+  };
+
+  renderingEngine.enableElement(viewportInput);
+
+  // Set the tool group on the viewport
+  toolGroup.addViewport(viewportId, renderingEngineId);
+
+  // Get the stack viewport that was created
+  const viewport = <Types.IStackViewport>(
+    renderingEngine.getViewport(viewportId)
   );
 
-  const isMobile = window.matchMedia('(any-pointer:coarse)').matches;
+  // Define a stack containing a single image
+  const stack = imageIds;
+
+  // Set the stack on the viewport
+  viewport.setStack(stack, 0);
 
   // Render the image
-  renderingEngine.renderViewports([viewportId1, viewportId2, viewportId3]);
+  viewport.render();
 }
 
-run(
-  Math.random(),
-  '1.3.6.1.4.1.14519.5.2.1.7009.2403.334240657131972136850343327463',
-  '1.3.6.1.4.1.14519.5.2.1.7009.2403.226151125820845824875394858561',
-  'https://d3t6nz73ql33tx.cloudfront.net/dicomweb'
-);
+run();
