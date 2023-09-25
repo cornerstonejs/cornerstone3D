840603cd3c48d5e8b0ca78bb9bf171f5c8c0c2a9
Alireza <ar.sedghi@gmail.com>
2023-09-25 14:36:28 -0400

    refactor and clean

--- packages/tools/src/tools/OverlayGridTool.ts
+++ packages/tools/src/tools/OverlayGridTool.ts
@@ -17,16 +17,20 @@ import { getToolGroup } from '../store/ToolGroupManager';
 import { drawLine as drawLineSvg } from '../drawingSvg';
 import triggerAnnotationRenderForViewportIds from '../utilities/triggerAnnotationRenderForViewportIds';
 
-import { PublicToolProps, ToolProps, SVGDrawingHelper } from '../types';
+import {
+  PublicToolProps,
+  ToolProps,
+  SVGDrawingHelper,
+  Annotation,
+} from '../types';
 import { StyleSpecifier } from '../types/AnnotationStyle';
 import AnnotationDisplayTool from './base/AnnotationDisplayTool';
-import { Annotation } from '../types';
 
 const { EPSILON } = CONSTANTS;
 
 export interface OverlayGridAnnotation extends Annotation {
   data: {
-    viewportGridInfoMap: Map<string, object>;
+    viewportData: Map<string, object>;
     pointSets: Array<object>;
   };
 }
@@ -55,54 +59,6 @@ class OverlayGridTool extends AnnotationDisplayTool {
     super(toolProps, defaultToolProps);
   }
 
-  _init = (): void => {
-    if (this.configuration.sourceImageIds?.length) {
-      const { frameOfReferenceUID } = metaData.get(
-        'imagePlaneModule',
-        this.configuration.sourceImageIds[0]
-      );
-      const viewportsInfo = getToolGroup(this.toolGroupId).viewportsInfo;
-
-      const annotations = getAnnotations(
-        this.getToolName(),
-        frameOfReferenceUID
-      );
-
-      if (!annotations?.length) {
-        const sourceImageIds = this.configuration.sourceImageIds;
-        const pointSets = [];
-        for (let i = 0; i < sourceImageIds.length; i++) {
-          // check if pointSets for the imageId was calculated. If not calculate and store
-          pointSets[i] = this.calculateImageIdPointSets(sourceImageIds[i]);
-        }
-
-        const newAnnotation: OverlayGridAnnotation = {
-          highlighted: true,
-          invalidated: true,
-          metadata: {
-            toolName: this.getToolName(),
-            FrameOfReferenceUID: frameOfReferenceUID,
-            referencedImageId: null,
-          },
-          data: {
-            viewportGridInfoMap: new Map(),
-            pointSets,
-          },
-        };
-
-        addAnnotation(newAnnotation, frameOfReferenceUID);
-      }
-      if (!viewportsInfo?.length) {
-        return;
-      }
-
-      triggerAnnotationRenderForViewportIds(
-        getRenderingEngine(viewportsInfo[0].renderingEngineId),
-        viewportsInfo.map(({ viewportId }) => viewportId)
-      );
-    }
-  };
-
   onSetToolEnabled = (): void => {
     this._init();
   };
@@ -111,9 +67,70 @@ class OverlayGridTool extends AnnotationDisplayTool {
     this._init();
   };
 
+  _init = (): void => {
+    const sourceImageIds = this.configuration.sourceImageIds;
+    if (!sourceImageIds?.length) {
+      console.warn(
+        'OverlayGridTool: No sourceImageIds provided in configuration'
+      );
+      return;
+    }
+
+    const imagePlaneModule = metaData.get(
+      'imagePlaneModule',
+      sourceImageIds[0]
+    );
+
+    if (!imagePlaneModule) {
+      console.warn(
+        'OverlayGridTool: No imagePlaneModule found for sourceImageIds'
+      );
+      return;
+    }
+
+    const { frameOfReferenceUID } = imagePlaneModule;
+
+    const viewportsInfo = getToolGroup(this.toolGroupId).viewportsInfo;
+
+    if (!viewportsInfo?.length) {
+      console.warn('OverlayGridTool: No viewports found');
+      return;
+    }
+
+    const annotations = getAnnotations(this.getToolName(), frameOfReferenceUID);
+
+    if (!annotations?.length) {
+      const pointSets = sourceImageIds.map((id) => {
+        // check if pointSets for the imageId was calculated. If not calculate and store
+        return this.calculateImageIdPointSets(id);
+      });
+
+      const newAnnotation: OverlayGridAnnotation = {
+        highlighted: true,
+        invalidated: true,
+        metadata: {
+          toolName: this.getToolName(),
+          FrameOfReferenceUID: frameOfReferenceUID,
+          referencedImageId: null,
+        },
+        data: {
+          viewportData: new Map(),
+          pointSets,
+        },
+      };
+
+      addAnnotation(newAnnotation, frameOfReferenceUID);
+    }
+
+    triggerAnnotationRenderForViewportIds(
+      getRenderingEngine(viewportsInfo[0].renderingEngineId),
+      viewportsInfo.map(({ viewportId }) => viewportId)
+    );
+  };
+
   /**
    * Calculates the point sets based on the image corners relative to an imageId
-   * @param imageId
+   * @param imageId - The imageId to calculate the point sets for
    * @returns
    */
   calculateImageIdPointSets = (imageId: string) => {
@@ -172,8 +189,10 @@ class OverlayGridTool extends AnnotationDisplayTool {
     enabledElement: Types.IEnabledElement,
     svgDrawingHelper: SVGDrawingHelper
   ): boolean => {
+    const sourceImageIds = this.configuration.sourceImageIds;
+
     let renderStatus = false;
-    if (!this.configuration.sourceImageIds?.length) {
+    if (!sourceImageIds?.length) {
       return renderStatus;
     }
 
@@ -184,13 +203,12 @@ class OverlayGridTool extends AnnotationDisplayTool {
     }
 
     const annotations = getAnnotations(this.getToolName(), FrameOfReferenceUID);
-    if (!annotations) {
+    if (!annotations?.length) {
       return renderStatus;
     }
     const annotation = annotations[0];
     const { annotationUID } = annotation;
 
-    const sourceImageIds = this.configuration.sourceImageIds;
     const { focalPoint, viewPlaneNormal } = targetViewport.getCamera();
 
     const styleSpecifier: StyleSpecifier = {
@@ -213,21 +231,17 @@ class OverlayGridTool extends AnnotationDisplayTool {
     );
 
     const pointSets = annotation.data.pointSets;
-    const viewportGridInfoMap = annotation.data.viewportGridInfoMap;
+    const viewportData = annotation.data.viewportData;
     for (let i = 0; i < sourceImageIds.length; i++) {
       // check if pointSets for the imageId was calculated. If not calculate and store
       const { pointSet1, pointSet2 } = pointSets[i];
 
-      if (!viewportGridInfoMap.get(targetViewport.id)) {
-        viewportGridInfoMap.set(targetViewport.id, {
-          pointSetsToUse: [],
-          lineStartsWorld: [],
-          lineEndsWorld: [],
-        });
-      }
+      const targetData =
+        viewportData.get(targetViewport.id) ||
+        this.initializeViewportData(viewportData, targetViewport.id);
 
       // check if pointSetToUse was calculated. If not calculate and store
-      if (!viewportGridInfoMap.get(targetViewport.id).pointSetsToUse[i]) {
+      if (!targetData.pointSetsToUse[i]) {
         let pointSetToUse = pointSet1;
 
         let topBottomVec = vec3.subtract(
@@ -240,41 +254,30 @@ class OverlayGridTool extends AnnotationDisplayTool {
           topBottomVec
         ) as Types.Point3;
 
-        let topRightVec = vec3.subtract(
-          vec3.create(),
-          pointSet1[2],
-          pointSet1[0]
-        );
-        topRightVec = vec3.normalize(vec3.create(), topRightVec);
-
         // check if it is perpendicular to the viewPlaneNormal which means
         // the line does not intersect the viewPlaneNormal
         if (this.isPerpendicular(topBottomVec, viewPlaneNormal)) {
           // 'use pointSet2';
           pointSetToUse = pointSet2;
         }
-        viewportGridInfoMap.get(targetViewport.id).pointSetsToUse[i] =
-          pointSetToUse;
 
-        viewportGridInfoMap.get(targetViewport.id).lineStartsWorld[i] =
-          csUtils.planar.linePlaneIntersection(
-            pointSetToUse[0],
-            pointSetToUse[1],
-            targetViewportPlane
-          );
+        targetData.pointSetsToUse[i] = pointSetToUse;
 
-        viewportGridInfoMap.get(targetViewport.id).lineEndsWorld[i] =
-          csUtils.planar.linePlaneIntersection(
-            pointSetToUse[2],
-            pointSetToUse[3],
-            targetViewportPlane
-          );
+        targetData.lineStartsWorld[i] = csUtils.planar.linePlaneIntersection(
+          pointSetToUse[0],
+          pointSetToUse[1],
+          targetViewportPlane
+        );
+
+        targetData.lineEndsWorld[i] = csUtils.planar.linePlaneIntersection(
+          pointSetToUse[2],
+          pointSetToUse[3],
+          targetViewportPlane
+        );
       }
 
-      const lineStartWorld = viewportGridInfoMap.get(targetViewport.id)
-        .lineStartsWorld[i];
-      const lineEndWorld = viewportGridInfoMap.get(targetViewport.id)
-        .lineEndsWorld[i];
+      const lineStartWorld = targetData.lineStartsWorld[i];
+      const lineEndWorld = targetData.lineEndsWorld[i];
 
       styleSpecifier.annotationUID = annotationUID;
       const lineWidth = this.getStyle('lineWidth', styleSpecifier, annotation);
@@ -309,16 +312,29 @@ class OverlayGridTool extends AnnotationDisplayTool {
     return renderStatus;
   };
 
-  isPerpendicular = (vec1: Types.Point3, vec2: Types.Point3): boolean => {
+  private initializeViewportData = (viewportData, id) => {
+    viewportData.set(id, {
+      pointSetsToUse: [],
+      lineStartsWorld: [],
+      lineEndsWorld: [],
+    });
+
+    return viewportData.get(id);
+  };
+
+  private isPerpendicular = (
+    vec1: Types.Point3,
+    vec2: Types.Point3
+  ): boolean => {
     const dot = vec3.dot(vec1, vec2);
     return Math.abs(dot) < EPSILON;
   };
 
-  isParallel(vec1: Types.Point3, vec2: Types.Point3): boolean {
+  private isParallel(vec1: Types.Point3, vec2: Types.Point3): boolean {
     return Math.abs(vec3.dot(vec1, vec2)) > 1 - EPSILON;
   }
 
-  getImageIdNormal(imageId: string): vec3 {
+  private getImageIdNormal(imageId: string): vec3 {
     const { imageOrientationPatient } = metaData.get(
       'imagePlaneModule',
       imageId
