3c87a4e0fe2aaf97484f9ddccfe60b5d011c3cf6
Bill Wallace <wayfarer3130@gmail.com>
2023-09-28 16:01:55 -0400

    Initial volume progressive loader

---
+++ packages/tools/examples/volumeProgressive/index.ts
@@ -0,0 +1,244 @@
+import {
+  RenderingEngine,
+  Types,
+  Enums,
+  volumeLoader,
+  setVolumesForViewports,
+} from '@cornerstonejs/core';
+import {
+  initDemo,
+  createImageIdsAndCacheMetaData,
+  setTitleAndDescription,
+  addToggleButtonToToolbar,
+} from '../../../../utils/demo/helpers';
+import * as cornerstoneTools from '@cornerstonejs/tools';
+
+// This is for debugging purposes
+console.warn(
+  'Click on index.ts to open source code for this example --------->'
+);
+
+const {
+  PanTool,
+  WindowLevelTool,
+  ZoomTool,
+  ToolGroupManager,
+  StackScrollMouseWheelTool,
+  Enums: csToolsEnums,
+  synchronizers,
+  SynchronizerManager,
+} = cornerstoneTools;
+
+const { ViewportType } = Enums;
+const { MouseBindings } = csToolsEnums;
+
+const { createCameraPositionSynchronizer, createVOISynchronizer } =
+  synchronizers;
+
+// Define a unique id for the volume
+const volumeName = 'CT_VOLUME_ID'; // Id of the volume less loader prefix
+const volumeLoaderScheme = 'cornerstoneStreamingImageVolume'; // Loader id which defines which volume loader to use
+const volumeId = `${volumeLoaderScheme}:${volumeName}`; // VolumeId with loader id + volume id
+
+const cameraSynchronizerId = 'CAMERA_SYNCHRONIZER_ID';
+const voiSynchronizerId = 'VOI_SYNCHRONIZER_ID';
+
+const renderingEngineId = 'myRenderingEngine';
+const viewportIds = [
+  'CT_SAGITTAL_STACK_1',
+  'CT_SAGITTAL_STACK_2',
+  'CT_SAGITTAL_STACK_3',
+];
+
+// ======== Set up page ======== //
+setTitleAndDescription(
+  'Progressive Load for Volume Viewport',
+  'Here we demonstrate progressive loading of volumes.'
+);
+
+const size = '500px';
+const content = document.getElementById('content');
+
+const loaders = document.createElement('div');
+content.appendChild(loaders);
+
+const timingInfo = document.createElement('div');
+timingInfo.innerText = 'Timing Info';
+content.appendChild(timingInfo);
+const viewportGrid = document.createElement('div');
+
+viewportGrid.style.display = 'flex';
+viewportGrid.style.display = 'flex';
+viewportGrid.style.flexDirection = 'row';
+
+const element1 = document.createElement('div');
+const element2 = document.createElement('div');
+const element3 = document.createElement('div');
+element1.style.width = size;
+element1.style.height = size;
+element2.style.width = size;
+element2.style.height = size;
+element3.style.width = size;
+element3.style.height = size;
+
+// Disable right click context menu so we can have right click tools
+element1.oncontextmenu = (e) => e.preventDefault();
+// Disable right click context menu so we can have right click tools
+element2.oncontextmenu = (e) => e.preventDefault();
+// Disable right click context menu so we can have right click tools
+element3.oncontextmenu = (e) => e.preventDefault();
+
+viewportGrid.appendChild(element1);
+viewportGrid.appendChild(element2);
+viewportGrid.appendChild(element3);
+
+content.appendChild(viewportGrid);
+
+const instructions = document.createElement('p');
+instructions.innerText = `
+Left Click to change window/level
+Use the mouse wheel to scroll through the stack.
+`;
+
+content.append(instructions);
+
+// ============================= //
+
+/**
+ * Runs the demo
+ */
+async function run() {
+  // Init Cornerstone and related libraries
+  await initDemo();
+
+  const toolGroupId = 'TOOL_GROUP_ID';
+
+  // Add tools to Cornerstone3D
+  cornerstoneTools.addTool(PanTool);
+  cornerstoneTools.addTool(WindowLevelTool);
+  cornerstoneTools.addTool(StackScrollMouseWheelTool);
+  cornerstoneTools.addTool(ZoomTool);
+
+  // Define a tool group, which defines how mouse events map to tool commands for
+  // Any viewport using the group
+  const toolGroup = ToolGroupManager.createToolGroup(toolGroupId);
+
+  // Add tools to the tool group
+  toolGroup.addTool(WindowLevelTool.toolName, { volumeId });
+  toolGroup.addTool(PanTool.toolName);
+  toolGroup.addTool(ZoomTool.toolName);
+  toolGroup.addTool(StackScrollMouseWheelTool.toolName);
+
+  // Set the initial state of the tools, here all tools are active and bound to
+  // Different mouse inputs
+  toolGroup.setToolActive(WindowLevelTool.toolName, {
+    bindings: [
+      {
+        mouseButton: MouseBindings.Primary, // Left Click
+      },
+    ],
+  });
+  toolGroup.setToolActive(PanTool.toolName, {
+    bindings: [
+      {
+        mouseButton: MouseBindings.Auxiliary, // Middle Click
+      },
+    ],
+  });
+  toolGroup.setToolActive(ZoomTool.toolName, {
+    bindings: [
+      {
+        mouseButton: MouseBindings.Secondary, // Right Click
+      },
+    ],
+  });
+  // As the Stack Scroll mouse wheel is a tool using the `mouseWheelCallback`
+  // hook instead of mouse buttons, it does not need to assign any mouse button.
+  toolGroup.setToolActive(StackScrollMouseWheelTool.toolName);
+
+  // Get Cornerstone imageIds and fetch metadata into RAM
+  const imageIds = await createImageIdsAndCacheMetaData({
+    StudyInstanceUID:
+      '1.3.6.1.4.1.14519.5.2.1.7009.2403.334240657131972136850343327463',
+    SeriesInstanceUID:
+      '1.3.6.1.4.1.14519.5.2.1.7009.2403.226151125820845824875394858561',
+    wadoRsRoot: 'https://d3t6nz73ql33tx.cloudfront.net/dicomweb',
+  });
+
+  // Instantiate a rendering engine
+  const renderingEngine = new RenderingEngine(renderingEngineId);
+
+  // Create the viewports
+  const viewportInputArray = [
+    {
+      viewportId: viewportIds[0],
+      type: ViewportType.ORTHOGRAPHIC,
+      element: element1,
+      defaultOptions: {
+        orientation: Enums.OrientationAxis.SAGITTAL,
+        background: <Types.Point3>[0.2, 0, 0.2],
+      },
+    },
+    {
+      viewportId: viewportIds[1],
+      type: ViewportType.ORTHOGRAPHIC,
+      element: element2,
+      defaultOptions: {
+        orientation: Enums.OrientationAxis.CORONAL,
+        background: <Types.Point3>[0.2, 0, 0.2],
+      },
+    },
+    {
+      viewportId: viewportIds[2],
+      type: ViewportType.ORTHOGRAPHIC,
+      element: element3,
+      defaultOptions: {
+        orientation: Enums.OrientationAxis.AXIAL,
+        background: <Types.Point3>[0.2, 0, 0.2],
+      },
+    },
+  ];
+
+  renderingEngine.setViewports(viewportInputArray);
+
+  // Set the tool group on the viewports
+  viewportIds.forEach((viewportId) =>
+    toolGroup.addViewport(viewportId, renderingEngineId)
+  );
+  renderingEngine.renderViewports(viewportIds);
+
+  async function loadVolume(name, imageIds) {
+    // Define a volume in memory
+    const key = `Volume Load ${name}`;
+    const start = Date.now();
+    console.time(key);
+    const volume = await volumeLoader.createAndCacheVolume(name, {
+      imageIds,
+    });
+    timingInfo.innerText = 'Loading...';
+
+    // Set the volume to load
+    volume.load((progress) => {
+      console.log('Load progress', progress);
+      if (progress.numFrames === progress.totalNumFrames) {
+        console.timeEnd(key);
+        const end = Date.now();
+        timingInfo.innerText = `Took ${end - start} ms for ${name} with ${
+          imageIds.length
+        } items`;
+      }
+    });
+
+    setVolumesForViewports(renderingEngine, [{ volumeId: name }], viewportIds);
+
+    // Render the image
+    renderingEngine.renderViewports(viewportIds);
+  }
+
+  const load1 = document.createElement('button');
+  load1.innerText = 'Load Random';
+  load1.onclick = loadVolume.bind(null, 'Random', imageIds);
+  loaders.appendChild(load1);
+}
+
+run();
