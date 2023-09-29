2974a991a9db646199fd778e2cd0a326fba794e5
Alireza <ar.sedghi@gmail.com>
2023-09-28 21:42:27 -0400

    fix a lot of dicom image loader circ deps

--- utils/demo/helpers/initCornerstoneDICOMImageLoader.js
+++ utils/demo/helpers/initCornerstoneDICOMImageLoader.js
@@ -5,36 +5,35 @@ import cornerstoneDICOMImageLoader from '@cornerstonejs/dicom-image-loader';
 
 window.cornerstone = cornerstone;
 window.cornerstoneTools = cornerstoneTools;
-const { preferSizeOverAccuracy, useNorm16Texture } =
-  cornerstone.getConfiguration().rendering;
 
 export default function initCornerstoneDICOMImageLoader() {
-  cornerstoneDICOMImageLoader.external.cornerstone = cornerstone;
-  cornerstoneDICOMImageLoader.external.dicomParser = dicomParser;
-  cornerstoneDICOMImageLoader.configure({
-    useWebWorkers: true,
-    decodeConfig: {
-      convertFloatPixelDataToInt: false,
-      use16BitDataType: preferSizeOverAccuracy || useNorm16Texture,
-    },
-  });
-
+  const { preferSizeOverAccuracy, useNorm16Texture } =
+    cornerstone.getConfiguration().rendering;
   let maxWebWorkers = 1;
 
   if (navigator.hardwareConcurrency) {
     maxWebWorkers = Math.min(navigator.hardwareConcurrency, 7);
   }
 
-  var config = {
-    maxWebWorkers,
-    startWebWorkersOnDemand: false,
-    taskConfiguration: {
-      decodeTask: {
-        initializeCodecsOnStartup: false,
-        strict: false,
+  cornerstoneDICOMImageLoader.init({
+    cornerstone,
+    dicomParser,
+    configuration: {
+      useWebWorkers: true,
+      decodeConfig: {
+        convertFloatPixelDataToInt: false,
+        use16BitDataType: preferSizeOverAccuracy || useNorm16Texture,
+      },
+      webWorkers: {
+        maxWebWorkers,
+        startWebWorkersOnDemand: false,
+        taskConfiguration: {
+          decodeTask: {
+            initializeCodecsOnStartup: false,
+            strict: false,
+          },
+        },
       },
     },
-  };
-
-  cornerstoneDICOMImageLoader.webWorkerManager.initialize(config);
+  });
 }
