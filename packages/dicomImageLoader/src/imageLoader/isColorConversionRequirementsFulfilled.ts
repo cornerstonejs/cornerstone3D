f133ace9efa3d57287110baaab724c9850fc7c5f
rodrigobasilio2022 <rodrigo.basilio@radicalimaging.com>
2023-10-18 06:50:28 -0300

    fix(color): fix bug in color example

--- packages/dicomImageLoader/src/imageLoader/isColorConversionRequirementsFulfilled.ts
+++ packages/dicomImageLoader/src/imageLoader/isColorConversionRequirementsFulfilled.ts
@@ -10,13 +10,19 @@ export default function isColorConversionRequirementsFulfilled(
   RGBA
 ) {
   const { rows, columns } = imageFrame;
-  if (
-    imageFrame.photometricInterpretation === 'YBR_FULL_422' &&
-    imageFrame.pixelDataLength === 2 * rows * columns &&
-    rows % 2 === 0
-  ) {
+  if (imageFrame.photometricInterpretation === 'RGB') {
+    return imageFrame.length % 3 === 0;
+  } else if (imageFrame.photometricInterpretation === 'YBR_RCT') {
+    return imageFrame.length % 3 === 0;
+  } else if (imageFrame.photometricInterpretation === 'YBR_ICT') {
+    return imageFrame.length % 3 === 0;
+  } else if (imageFrame.photometricInterpretation === 'PALETTE COLOR') {
     return true;
+  } else if (imageFrame.photometricInterpretation === 'YBR_FULL_422') {
+    return imageFrame.pixelDataLength === 2 * rows * columns && rows % 2 === 0;
+  } else if (imageFrame.photometricInterpretation === 'YBR_FULL') {
+    return imageFrame.length % 3 === 0;
   } else {
-    return false;
+    return true;
   }
 }
