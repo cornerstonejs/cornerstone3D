d5476e7ac90fc96bea7bafb1399fd50f960a65c9
Alireza <ar.sedghi@gmail.com>
2023-11-14 16:31:09 -0500

    Refactor pointInEllipse function to accept optional inverts parameter

--- packages/tools/src/utilities/math/ellipse/pointInEllipse.ts
+++ packages/tools/src/utilities/math/ellipse/pointInEllipse.ts
@@ -1,40 +1,37 @@
-import type { Types } from '@cornerstonejs/core';
-
-type Ellipse = {
-  center: Types.Point3;
-  xRadius: number;
-  yRadius: number;
-  zRadius: number;
-};
-
 /**
  * Given an ellipse and a point, return true if the point is inside the ellipse
  * @param ellipse - The ellipse object to check against.
  * @param pointLPS - The point in LPS space to test.
  * @returns A boolean value.
  */
-export default function pointInEllipse(ellipse, pointLPS) {
+export default function pointInEllipse(ellipse, pointLPS, inverts) {
   const { center, xRadius, yRadius, zRadius } = ellipse;
 
-  // Precompute inverses of the radii squared if they are not zero
-  const invXRadiusSq = xRadius !== 0 ? 1 / xRadius ** 2 : 0;
-  const invYRadiusSq = yRadius !== 0 ? 1 / yRadius ** 2 : 0;
-  const invZRadiusSq = zRadius !== 0 ? 1 / zRadius ** 2 : 0;
+  let invXRadiusSqToUse, invYRadiusSqToUse, invZRadiusSqToUse;
+  if (inverts !== undefined) {
+    invXRadiusSqToUse = inverts.invXRadiusSq;
+    invYRadiusSqToUse = inverts.invYRadiusSq;
+    invZRadiusSqToUse = inverts.invZRadiusSq;
+  } else {
+    invXRadiusSqToUse = xRadius !== 0 ? 1 / xRadius ** 2 : 0;
+    invYRadiusSqToUse = yRadius !== 0 ? 1 / yRadius ** 2 : 0;
+    invZRadiusSqToUse = zRadius !== 0 ? 1 / zRadius ** 2 : 0;
+  }
 
   let inside = 0;
 
   // Calculate the sum of normalized squared distances
-  inside += (pointLPS[0] - center[0]) ** 2 * invXRadiusSq;
+  inside += (pointLPS[0] - center[0]) ** 2 * invXRadiusSqToUse;
   if (inside > 1) {
     return false;
   }
 
-  inside += (pointLPS[1] - center[1]) ** 2 * invYRadiusSq;
+  inside += (pointLPS[1] - center[1]) ** 2 * invYRadiusSqToUse;
   if (inside > 1) {
     return false;
   }
 
-  inside += (pointLPS[2] - center[2]) ** 2 * invZRadiusSq;
+  inside += (pointLPS[2] - center[2]) ** 2 * invZRadiusSqToUse;
 
   // Check if the point is inside the ellipse
   return inside <= 1;
