54b4255b09283846f85f52b654d11092a6b70958
Alireza <ar.sedghi@gmail.com>
2023-11-15 10:35:41 -0500

    Add @babel/plugin-transform-class-static-block to Babel config and package.json

--- babel.config.js
+++ babel.config.js
@@ -11,6 +11,7 @@ module.exports = (api) => {
       '@babel/plugin-proposal-class-properties',
       '@babel/plugin-transform-runtime',
       '@babel/plugin-transform-typescript',
+      '@babel/plugin-transform-class-static-block',
     ],
   };
 };
