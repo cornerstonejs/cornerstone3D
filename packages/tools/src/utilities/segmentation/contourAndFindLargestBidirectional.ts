eb56621e24d2c73bc9aece28211c8d6f80243ce1
Bill Wallace <wayfarer3130@gmail.com>
2023-12-19 08:22:32 -0500

    Performance enhancements

--- packages/tools/src/utilities/segmentation/contourAndFindLargestBidirectional.ts
+++ packages/tools/src/utilities/segmentation/contourAndFindLargestBidirectional.ts
@@ -31,11 +31,9 @@ const { Labelmap } = SegmentationRepresentations;
  * @param segmentations.segments.color - the color to use for the segment label
  */
 export default function contourAndFindLargestBidirectional(segmentations) {
-  console.time('contour');
   const contours = generateContourSetsFromLabelmap({
     segmentations,
   });
-  console.timeEnd('contour');
 
   if (!contours?.length || !contours[0].sliceContours.length) {
     return;
@@ -82,17 +80,29 @@ function createIsInSegment(
   const width = vol.dimensions[0];
   const pixelsPerSlice = width * vol.dimensions[1];
 
-  /**
-   * Find the center point between point1 and point2, convert it to IJK space
-   * and test if the value at that location is in the segment
-   */
-  return (point1, point2) => {
-    const point = vec3.add(vec3.create(), point1, point2).map((it) => it / 2);
-    const ijk = vol.imageData.worldToIndex(point as vec3).map(Math.round);
-    const [i, j, k] = ijk;
-    const index = i + j * width + k * pixelsPerSlice;
-    const value = segData[index];
-    return value === segmentIndex || containedSegmentIndices?.has(value);
+  return {
+    /**
+     * Find the center point between point1 and point2, convert it to IJK space
+     * and test if the value at that location is in the segment
+     */
+    testCenter: (point1, point2) => {
+      const point = vec3.add(vec3.create(), point1, point2).map((it) => it / 2);
+      const ijk = vol.imageData.worldToIndex(point as vec3).map(Math.round);
+      const [i, j, k] = ijk;
+      const index = i + j * width + k * pixelsPerSlice;
+      const value = segData[index];
+      return value === segmentIndex || containedSegmentIndices?.has(value);
+    },
+
+    toIJK: (point) => vol.imageData.worldToIndex(point as vec3),
+
+    testIJK: (ijk) => {
+      const [i, j, k] = ijk;
+      const index =
+        Math.round(i) + Math.round(j) * width + Math.round(k) * pixelsPerSlice;
+      const value = segData[index];
+      return value === segmentIndex || containedSegmentIndices?.has(value);
+    },
   };
 }
 
@@ -125,23 +135,15 @@ function findLargestBidirectional(contours, segVolumeId: string, segment) {
     containedSegmentIndices
   );
   for (const sliceContour of sliceContours) {
-    const startTimeMs = Date.now();
     const bidirectional = createBidirectionalForSlice(
       sliceContour,
       isInSegment,
       maxBidirectional
     );
-    const durationMs = Date.now() - startTimeMs;
     if (!bidirectional) {
-      if (durationMs > 250) {
-        console.log('Skipped slice bidirectional took', durationMs);
-      }
       continue;
     }
     maxBidirectional = bidirectional;
-    if (durationMs > 250) {
-      console.log('Slice bidirectional calculation took', durationMs);
-    }
   }
   if (maxBidirectional) {
     Object.assign(maxBidirectional, segment);
@@ -155,54 +157,36 @@ const lengthSq = (point) =>
 const distanceSq = (p1, p2) => lengthSq(vec3.sub(vec3.create(), p1, p2));
 
 /**
- * Determines if there is a point in points other than those referenced by the
- * two indices, which is on the line (or nearly so, to deal with quantization issues)
- * between the points referenced in the two indices.
- * The algorithm used is to first check the boundary region of point1/point2 to
- * exclude obvious outside boundary points, then
- * compare the distances between point1 and the   test point
- * and between the test point and point2 to see if they add up to distance, and
- * are thus a straight line.
- *
- * Uses the values in the map point1Lengths to speed up the distance calculations
+ * Determines if there is a point between point1 and point2 which is not
+ * contained in the segmentation
  */
-function isCrossing(
-  points,
-  point1Index,
-  point2Index,
-  distance,
-  point1Lengths: Map<number, number>
+function isLineInSegment(
+  point1: Types.Point3,
+  point2: Types.Point3,
+  isInSegment
 ) {
-  const point1 = points[point1Index];
-  const point2 = points[point2Index];
-  const pointMax = point1.map((it, index) => Math.max(it, point2[index]));
-  const pointMin = point1.map((it, index) => Math.min(it, point2[index]));
-  const pointsLength = points.length;
-  for (let testIndex = 0; testIndex < pointsLength; testIndex++) {
-    if (testIndex === point1Index || testIndex === point2Index) {
-      continue;
-    }
-    const point = points[testIndex];
-    if (
-      point.find((v, index) => v < pointMin[index] || v > pointMax[index]) !==
-      undefined
-    ) {
-      // Points entirely outside should be skipped
-      continue;
-    }
-    const point1Length = point1Lengths.get(testIndex);
-    const length1 = point1Length ?? vec3.distance(point1, point);
-    if (point1Length === undefined) {
-      point1Lengths.set(testIndex, length1);
-    }
-    // Storing the additional lengths for point2 becomes quite expensive,
-    // in terms of memory, so probably not worth doing right now.
-    const length2 = vec3.distance(point, point2);
-    if (Math.abs(distance - length1 - length2) < 5 * EPSILON) {
-      return true;
+  const ijk1 = isInSegment.toIJK(point1);
+  const ijk2 = isInSegment.toIJK(point2);
+  const testPoint = vec3.create();
+  const { testIJK } = isInSegment;
+  const delta = vec3.sub(vec3.create(), ijk1, ijk2);
+
+  // Test once for index value between the two points, so the max of the
+  // difference in IJK values
+  const testSize = Math.round(Math.max(...delta.map(Math.abs)));
+  if (testSize < 2) {
+    // No need to test when there are only two points
+    return true;
+  }
+  const unitDelta = vec3.scale(vec3.create(), delta, 1 / testSize);
+
+  for (let i = 1; i < testSize; i++) {
+    vec3.scaleAndAdd(testPoint, ijk2, unitDelta, i);
+    if (!testIJK(testPoint)) {
+      return false;
     }
   }
-  return false;
+  return true;
 }
 
 /**
@@ -225,7 +209,6 @@ function createBidirectionalForSlice(
   let maxMinor = currentMaxMinor * currentMaxMinor;
   let maxMajorPoints;
   for (let index1 = 0; index1 < points.length; index1++) {
-    const point1Lengths = new Map<number, number>();
     for (let index2 = index1 + 1; index2 < points.length; index2++) {
       const point1 = points[index1];
       const point2 = points[index2];
@@ -238,13 +221,12 @@ function createBidirectionalForSlice(
         // so that all minor axis can be tested
         continue;
       }
-      if (!isInSegment(point1, point2)) {
+      if (!isInSegment.testCenter(point1, point2)) {
         // Center between the two points has to be in the segment, otherwise
         // this is out of bounds.
         continue;
       }
-      const distance = point1Lengths.get(index2) ?? Math.sqrt(distance2);
-      if (isCrossing(points, index1, index2, distance, point1Lengths)) {
+      if (!isLineInSegment(point1, point2, isInSegment)) {
         // If the line intersects the segment boundary, then skip it
         continue;
       }
@@ -260,13 +242,12 @@ function createBidirectionalForSlice(
   maxMajor = Math.sqrt(maxMajor);
   const handle0 = points[maxMajorPoints[0]];
   const handle1 = points[maxMajorPoints[1]];
-  const majorDelta = vec3.sub(vec3.create(), handle0, handle1);
-  majorDelta.forEach((it, index) => (majorDelta[index] = it / maxMajor));
+  const unitMajor = vec3.sub(vec3.create(), handle0, handle1);
+  vec3.scale(unitMajor, unitMajor, 1 / maxMajor);
 
   let maxMinorPoints;
 
   for (let index1 = 0; index1 < points.length; index1++) {
-    const point1Lengths = new Map<number, number>();
     for (let index2 = index1 + 1; index2 < points.length; index2++) {
       const point1 = points[index1];
       const point2 = points[index2];
@@ -274,19 +255,19 @@ function createBidirectionalForSlice(
       if (distance2 <= maxMinor) {
         continue;
       }
-      const distance = point1Lengths.get(index2) ?? Math.sqrt(distance2);
       const delta = vec3.sub(vec3.create(), point1, point2);
 
-      const dot = Math.abs(vec3.dot(delta, majorDelta)) / distance;
+      const dot = Math.abs(vec3.dot(delta, unitMajor)) / Math.sqrt(distance2);
       if (dot > EPSILON) {
         continue;
       }
-      if (!isInSegment(point1, point2)) {
+
+      if (!isInSegment.testCenter(point1, point2)) {
         // Center between the two points has to be in the segment, otherwise
         // this is out of bounds.
         continue;
       }
-      if (isCrossing(points, index1, index2, distance, point1Lengths)) {
+      if (!isLineInSegment(point1, point2, isInSegment)) {
         continue;
       }
       maxMinor = distance2;
