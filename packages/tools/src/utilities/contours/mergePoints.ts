ba32b913ddbd723e31308868b2327f1850f88692
Alireza <ar.sedghi@gmail.com>
2024-01-16 15:15:40 -0500

    Refactor mergePoints.ts to remove duplicate points and update point references in lines array

--- packages/tools/src/utilities/contours/mergePoints.ts
+++ packages/tools/src/utilities/contours/mergePoints.ts
@@ -1,106 +1,59 @@
 /**
- * Checks if point is within array
- * @param {*} array
- * @param {*} pt
- * @returns
- */
-function ptInArray(array, pt) {
-  let index = -1;
-  for (let i = 0; i < array.length; i++) {
-    if (isSamePoint(pt, array[i])) {
-      index = i;
-    }
-  }
-  return index;
-}
-
-/**
- * Checks if point A and point B contain same values
- * @param {*} ptA
- * @param {*} ptB
- * @returns
- */
-function isSamePoint(ptA, ptB) {
-  if (ptA[0] == ptB[0] && ptA[1] == ptB[1] && ptA[2] == ptB[2]) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-/**
- * Goes through linesArray and replaces all references of old index with new index
- * @param {*} linesArray
- * @param {*} oldIndex
- * @param {*} newIndex
- */
-function replacePointIndexReferences(linesArray, oldIndex, newIndex) {
-  for (let i = 0; i < linesArray.length; i++) {
-    const line = linesArray[i];
-    if (line.a == oldIndex) {
-      line.a = newIndex;
-    } else if (line.b == oldIndex) {
-      line.b = newIndex;
-    }
-  }
-}
-
-/**
  * Iterate through polyData from vtkjs and merge any points that are the same
  * then update merged point references within lines array
- * @param {*} polyData
- * @param {*} bypass
- * @returns
+ * @param polyData - vtkPolyData
+ * @param bypass - bypass the duplicate point removal
+ * @returns the updated polyData
  */
 export function removeDuplicatePoints(polyData, bypass = false) {
   const points = polyData.getPoints();
   const lines = polyData.getLines();
 
-  const pointsArray = [];
-  for (let i = 0; i < points.getNumberOfPoints(); i++) {
-    const pt = points.getPoint(i).slice();
-    pointsArray.push(pt);
-  }
-  const linesArray = [];
-  for (let i = 0; i < lines.getNumberOfCells(); i++) {
+  const pointsArray = new Array(points.getNumberOfPoints())
+    .fill(0)
+    .map((_, i) => points.getPoint(i).slice());
+
+  const linesArray = new Array(lines.getNumberOfCells()).fill(0).map((_, i) => {
     const cell = lines.getCell(i * 3).slice();
-    //console.log(JSON.stringify(cell));
-    const a = cell[0];
-    const b = cell[1];
-    const line = {
-      a,
-      b,
-    };
-    linesArray.push(line);
-  }
+    return { a: cell[0], b: cell[1] };
+  });
 
   if (bypass) {
     return { points: pointsArray, lines: linesArray };
   }
 
-  // Iterate through points and replace any duplicates
   const newPoints = [];
-  for (let i = 0; i < pointsArray.length; i++) {
-    const pt = pointsArray[i];
-    let index = ptInArray(newPoints, pt);
+  for (const [i, pt] of pointsArray.entries()) {
+    const index = newPoints.findIndex(
+      (point) => point[0] === pt[0] && point[1] === pt[1] && point[2] === pt[2]
+    );
 
     if (index >= 0) {
-      // Duplicate Point -> replace references in lines
-      replacePointIndexReferences(linesArray, i, index);
+      linesArray.map((line) => {
+        if (line.a === i) {
+          line.a = index;
+        }
+        if (line.b === i) {
+          line.b = index;
+        }
+        return line;
+      });
     } else {
-      index = newPoints.length;
+      const newIndex = newPoints.length;
       newPoints.push(pt);
-      replacePointIndexReferences(linesArray, i, index);
+      linesArray.map((line) => {
+        if (line.a === i) {
+          line.a = newIndex;
+        }
+        if (line.b === i) {
+          line.b = newIndex;
+        }
+        return line;
+      });
     }
   }
 
-  // Final pass through lines, remove any that refer to exact same point
-  const newLines = [];
-  linesArray.forEach((line) => {
-    if (line.a != line.b) {
-      newLines.push(line);
-    }
-  });
+  const newLines = linesArray.filter((line) => line.a !== line.b);
 
   return { points: newPoints, lines: newLines };
 }
