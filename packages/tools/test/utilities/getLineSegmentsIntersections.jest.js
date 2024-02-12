64a59fd51d39c31883882199e7a58b7c071224be
Leonardo Campos <leonardoscampos@gmail.com>
2024-02-12 18:56:16 -0300

    fix: invalid reference to getLineSegmentIntersections in jest file

--- packages/tools/test/utilities/getLineSegmentsIntersections.jest.js
+++ packages/tools/test/utilities/getLineSegmentsIntersections.jest.js
@@ -1,4 +1,4 @@
-import getLineSegmentsIntersections from '../../src/utilities/math/polyline/getLineSegmentsIntersection';
+import getLinesIntersections from '../../src/utilities/math/polyline/getLinesIntersection';
 import { describe, it, expect } from '@jest/globals';
 
 const p11 = [1, 1];
@@ -7,37 +7,32 @@ const p01 = [0, 1];
 const p00 = [0, 0];
 const p22 = [2, 2];
 
-describe('getLineSegmentsIntersections', function () {
+describe('getLinesIntersections', function () {
   it('Should find overlapping intersection', () => {
-    const pTest = getLineSegmentsIntersections(p00, p11, p10, p01);
+    const pTest = getLinesIntersections(p00, p11, p10, p01);
     expect(pTest[0]).toBeCloseTo(0.5);
     expect(pTest[1]).toBeCloseTo(0.5);
   });
   it('Should find co-incident overlapping intersections', () => {
-    const pTest = getLineSegmentsIntersections(p00, p11, p11, p22);
+    const pTest = getLinesIntersections(p00, p11, p11, p22);
     expect(pTest[0]).toBeCloseTo(1);
     expect(pTest[1]).toBeCloseTo(1);
   });
 
   it('TODO Should find midpoint of parallel points', () => {
-    const pTest = getLineSegmentsIntersections(p00, p11, [0, -1], p10);
+    const pTest = getLinesIntersections(p00, p11, [0, -1], p10);
     expect(pTest[0]).toBeCloseTo(0.5);
     expect(pTest[1]).toBeCloseTo(0);
   });
 
   it('TODO Should find projected intersection of extended points', () => {
-    const pTest = getLineSegmentsIntersections(p00, p11, [2, 0], [3, -1]);
+    const pTest = getLinesIntersections(p00, p11, [2, 0], [3, -1]);
     expect(pTest[0]).toBeCloseTo(1);
     expect(pTest[1]).toBeCloseTo(1);
   });
 
   it('TODO Should find projected intersection of extended points', () => {
-    const pTest = getLineSegmentsIntersections(
-      p00,
-      p11,
-      [0, -1],
-      [1, 0.000000001]
-    );
+    const pTest = getLinesIntersections(p00, p11, [0, -1], [1, 0.000000001]);
     expect(pTest[0]).toBeCloseTo(999999917.2596358);
     expect(pTest[1]).toBeCloseTo(999999917.2596358);
   });
