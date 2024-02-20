4eabf8d5b01067278936889dafd1b1c25d2e52e0
Bill Wallace <wayfarer3130@gmail.com>
2024-02-20 10:10:05 -0500

    Change the video size for segmentation to match underlying size

--- packages/tools/examples/videoSegmentation/index.ts
+++ packages/tools/examples/videoSegmentation/index.ts
@@ -48,7 +48,7 @@ setTitleAndDescription(
   'Here we demonstrate how to render a labelmap based segmentation on a VideoViewport'
 );
 
-const size = '500px';
+const size = '1920px';
 const content = document.getElementById('content');
 const viewportGrid = document.createElement('div');
 
@@ -60,17 +60,9 @@ const element1 = document.createElement('div');
 element1.oncontextmenu = () => false;
 
 element1.style.width = size;
-element1.style.height = size;
+element1.style.height = '500px';
 
 viewportGrid.appendChild(element1);
-
-const element2 = document.createElement('div');
-element2.oncontextmenu = () => false;
-
-element2.style.width = size;
-element2.style.height = size;
-
-viewportGrid.appendChild(element2);
 
 content.appendChild(viewportGrid);
 
