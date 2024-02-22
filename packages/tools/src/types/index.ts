8ecd0b5814a932ec2b1eb0e07356bb7f1b6d1c1a
Bill Wallace <wayfarer3130@gmail.com>
2024-02-22 14:33:22 -0500

    feat: Add contour hole interpolation (#1112)
    * feat: Add contour hole interpolation
    
    * Tweak the handle position to deal with simplification
    
    * Fix the change of tool not working issue
    
    * Fix shift-click holes
    
    * Fix bindings for holes in more places
    
    * fix: Planar freeform ordering direction issues on interpolation
    
    * Move contour seg binding into helper function for now.

--- packages/tools/src/types/index.ts
+++ packages/tools/src/types/index.ts
@@ -23,7 +23,7 @@ import type {
   IToolBinding,
   ToolOptionsType,
 } from './ISetToolModeOptions';
-import type IToolGroup from './IToolGroup';
+import type IToolGroup from '../store/ToolGroupManager/ToolGroup';
 import type * as ToolSpecificAnnotationTypes from './ToolSpecificAnnotationTypes';
 import type * as AnnotationStyle from './AnnotationStyle';
 import type ToolHandle from './ToolHandle';
