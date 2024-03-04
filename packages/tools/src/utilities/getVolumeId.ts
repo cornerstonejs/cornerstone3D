b6c547183c4f84f4942b7c1ceca3a9544f6ba648
CYB3RL1F3 <cyberlife.music@gmail.com>
2024-03-04 20:39:29 +0100

    fix

--- packages/tools/src/utilities/getVolumeId.ts
+++ packages/tools/src/utilities/getVolumeId.ts
@@ -1,8 +1,13 @@
 export const getVolumeId = (targetId: string) => {
-  const str = targetId.substring('volumeId:'.length);
+  const prefix = 'volumeId:';
+  const str = targetId.includes(prefix)
+    ? targetId.substring(prefix.length)
+    : targetId;
+
   if (str.startsWith('nifti:')) {
     return str;
   }
+
   const index = str.indexOf('?');
   return index === -1 ? str : str.substring(0, index);
 };
--- packages/tools/test/getVolumeId_test.js
+++ packages/tools/test/getVolumeId_test.js
@@ -8,6 +8,15 @@ describe('getVolumeId', () => {
 
   it('should extract volumeId from a complex nifti url', () => {
     const targetId = 'volumeId:nifti:https://nifti.com/img.tar.nz?a=1&b=2';
+
+    expect(getVolumeId(targetId)).toEqual(
+      'nifti:https://nifti.com/img.tar.nz?a=1&b=2'
+    );
+  });
+
+  it('should extract volumeId from a complex nifti url without volumeId:', () => {
+    const targetId = 'nifti:https://nifti.com/img.tar.nz?a=1&b=2';
+
     expect(getVolumeId(targetId)).toEqual(
       'nifti:https://nifti.com/img.tar.nz?a=1&b=2'
     );
