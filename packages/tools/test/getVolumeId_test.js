779e855c428bdf3c85920a0b3f355679c1d768a0
Alireza <ar.sedghi@gmail.com>
2024-03-14 17:17:13 -0400

    tests

--- packages/tools/test/getVolumeId_test.js
+++ packages/tools/test/getVolumeId_test.js
@@ -3,7 +3,7 @@ import { getVolumeId } from '../../core/src/utilities/getVolumeId';
 describe('getVolumeId', () => {
   it('should extract volumeId from target', () => {
     const targetId = 'volumeId:123456';
-    expect(getVolumeId(targetId)).toEqual('volumeId:123456');
+    expect(getVolumeId(targetId)).toEqual('123456');
   });
   // it('should extract volumeId from a complex nifti url', () => {
   //   const targetId = 'volumeId:nifti:https://nifti.com/img.tar.nz?a=1&b=2';
