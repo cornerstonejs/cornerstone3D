798b8093b9ddb6b72951445c0261acb62322f8a5
Jorge <jlopes@ai4medimaging.com>
2024-04-02 14:41:31 +0100

    added createElement

---
+++ utils/demo/helpers/createElement.ts
@@ -0,0 +1,46 @@
+export type configElement = {
+  tag?: string;
+  class?: string;
+  attr?: Record<string, string>;
+  style?: Record<string, string>;
+  html?: string;
+  event?: Record<string, () => void>;
+  container?: HTMLElement;
+};
+
+export default function createElement(config: configElement = {}) {
+  const element = document.createElement(config.tag ?? 'div');
+
+  if (config.class) {
+    const splitted = config.class.split(' ');
+    splitted.forEach((item) => element.classList.add(item));
+  }
+
+  if (config.attr) {
+    for (const key in config.attr) {
+      element.setAttribute(key, config.attr[key]);
+    }
+  }
+
+  if (config.style) {
+    for (const key in config.style) {
+      element.style[key] = config.style[key];
+    }
+  }
+
+  if (config.html) {
+    element.innerHTML = config.html;
+  }
+
+  if (config.event) {
+    for (const key in config.event) {
+      element.addEventListener(key, config.event[key]);
+    }
+  }
+
+  if (config.container) {
+    config.container.append(element);
+  }
+
+  return element;
+}
