14c1fdc79aefe053aedf0a56b69efbb595b68f0a
Alireza <ar.sedghi@gmail.com>
2024-04-02 15:14:09 -0400

    Add segmentIndex parameter to thresholding functions

--- packages/tools/src/utilities/segmentation/rectangleROIThresholdVolumeByRange.ts
+++ packages/tools/src/utilities/segmentation/rectangleROIThresholdVolumeByRange.ts
@@ -13,6 +13,7 @@ export type ThresholdOptions = {
   numSlicesToProject?: number; // number of slices to project before and after current slice
   overwrite: boolean;
   overlapType?: number; // type of the voxel overlap
+  segmentIndex?: number; // segment index to threshold
 };
 
 export type AnnotationForThresholding = {
