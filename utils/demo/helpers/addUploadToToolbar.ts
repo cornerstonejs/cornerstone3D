dfd81780a8c41a5629c886b85e400b26ee17125b
Jorge <jlopes@ai4medimaging.com>
2024-04-03 00:47:57 +0100

    added addUploadToToolbar

---
+++ utils/demo/helpers/addUploadToToolbar.ts
@@ -0,0 +1,55 @@
+import createElement, { configElement } from './createElement';
+import addButtonToToolbar from './addButtonToToolbar';
+
+interface configButton extends configElement {
+  id?: string;
+  title: string;
+  container?: HTMLElement;
+  onChange: (files: FileList) => void;
+  input?: configElement;
+}
+
+export default function addUploadToToolbar(
+  config: configButton = {
+    title: undefined,
+    onChange: undefined,
+  }
+) {
+  config.container =
+    config.container ?? document.getElementById('demo-toolbar');
+
+  const fnClick = () => {
+    const elInput = createElement({
+      tag: 'input',
+      attr: {
+        type: 'file',
+        multiple: true,
+        hidden: true,
+      },
+      event: {
+        change: (evt: Event) => {
+          const files = (evt.target as HTMLInputElement).files;
+
+          config.onChange(files);
+
+          elInput.remove();
+        },
+        cancel: () => {
+          elInput.remove();
+        },
+      },
+      ...config.input,
+    });
+
+    document.body.appendChild(elInput);
+
+    elInput.click();
+  };
+
+  const button = addButtonToToolbar({
+    ...config,
+    onClick: fnClick,
+  });
+
+  return button;
+}
