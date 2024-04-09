885c4900339988db77b2e1a54efa5f0cbf746320
Alireza <ar.sedghi@gmail.com>
2024-04-09 15:43:09 -0400

    Fix lodash import in addSegmentations.ts

--- packages/tools/src/stateManagement/segmentation/addSegmentations.ts
+++ packages/tools/src/stateManagement/segmentation/addSegmentations.ts
@@ -1,4 +1,4 @@
-import cloneDeep from 'lodash.cloneDeep';
+import cloneDeep from 'lodash.clonedeep';
 import { SegmentationPublicInput } from '../../types/SegmentationStateTypes';
 import { validateSegmentationInput } from './helpers';
 import { addSegmentation as addSegmentationToState } from './segmentationState';
