19387d9a8e104b7ba17ea3e1647b375692a90864
Ibrahim <93064150+IbrahimCSAE@users.noreply.github.com>
2024-05-17 13:11:12 -0400

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,26 +22,12 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'], deviceScaleFactor: 1 },
     },
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'], deviceScaleFactor: 2 },
-    },
-
-    {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'], deviceScaleFactor: 1 },
     },
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'], deviceScaleFactor: 2 },
-    },
-
-    {
       name: 'webkit',
       use: { ...devices['Desktop Safari'], deviceScaleFactor: 1 },
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'], deviceScaleFactor: 2 },
     },
   ],
   webServer: {
