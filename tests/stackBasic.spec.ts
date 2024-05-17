6ff4a0310a53b554b4b4f1cfd9152d8214ebbfe4
Ibrahim <93064150+IbrahimCSAE@users.noreply.github.com>
2024-05-17 13:21:35 -0400

    feat(test): Playwright testing integration (#1250)

---
+++ tests/stackBasic.spec.ts
@@ -0,0 +1,19 @@
+import { test } from '@playwright/test';
+import {
+  visitExample,
+  checkForScreenshot,
+  screenShotPaths,
+} from './utils/index';
+
+test.beforeEach(async ({ page }) => {
+  await visitExample(page, 'stackBasic');
+});
+
+test.describe('Basic Stack', async () => {
+  test('should display a single DICOM image in a Stack viewport.', async ({
+    page,
+  }) => {
+    const locator = page.locator('.cornerstone-canvas');
+    await checkForScreenshot(locator, screenShotPaths.stackBasic.viewport);
+  });
+});
