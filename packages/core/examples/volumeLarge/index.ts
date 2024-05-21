60cad0e3405fd3290360971052eef7f10f77510f
Bill Wallace <wayfarer3130@gmail.com>
2024-05-21 09:48:43 -0400

    Added 16 texture render sample

--- packages/core/examples/volumeLarge/index.ts
+++ packages/core/examples/volumeLarge/index.ts
@@ -3,13 +3,17 @@ import {
   Types,
   Enums,
   volumeLoader,
-  CONSTANTS,
+  setConfiguration,
+  getConfiguration,
+  cache,
 } from '@cornerstonejs/core';
 import {
   initDemo,
   createImageIdsAndCacheMetaData,
   setTitleAndDescription,
   setCtTransferFunctionForVolumeActor,
+  addButtonToToolbar,
+  addToggleButtonToToolbar,
 } from '../../../../utils/demo/helpers';
 
 // This is for debugging purposes
@@ -25,6 +29,18 @@ setTitleAndDescription(
   'Displays a large volume in a viewport (>2 gb).'
 );
 
+// Define a unique id for the volume
+const volumeName = 'LargeVolume_Float32_Webassembly'; // Id of the volume less loader prefix
+const volumeName16 = 'LargeVolume_Float16'; // Id of hte volume using Float16
+const volumeLoaderScheme = 'cornerstoneStreamingImageVolume'; // Loader id which defines which volume loader to use
+const volumeId = `${volumeLoaderScheme}:${volumeName}`; // VolumeId with loader id + volume id
+const volumeId16 = `${volumeLoaderScheme}:${volumeName16}`;
+let imageIds, viewport, renderingEngine;
+const renderingEngineId = 'myRenderingEngine';
+const viewportId = 'CT_SAGITTAL_STACK';
+// 16 gb cache size
+const LargeCacheSize = 16 * 1024 * 1024 * 1024;
+
 const content = document.getElementById('content');
 const element = document.createElement('div');
 element.id = 'cornerstone-element';
@@ -33,6 +49,97 @@ element.style.height = '500px';
 
 content.appendChild(element);
 // ============================= //
+
+addToggleButtonToToolbar({
+  title: 'Load 16 Float',
+  defaultToggle: false,
+  onClick: (toggle) => {
+    if (toggle) {
+      loadImage16Float();
+    } else {
+      loadImage32Float();
+    }
+  },
+});
+
+async function loadImage32Float() {
+  console.log('Loading image 16 float');
+  renderingEngine.disableElement(viewportId);
+
+  const viewportInput = {
+    viewportId,
+    type: ViewportType.ORTHOGRAPHIC,
+    element,
+    defaultOptions: {
+      orientation: Enums.OrientationAxis.SAGITTAL,
+      background: <Types.Point3>[0.2, 0, 0.2],
+    },
+  };
+  renderingEngine.enableElement(viewportInput);
+
+  // Get the volume viewport that was created
+  viewport = <Types.IVolumeViewport>renderingEngine.getViewport(viewportId);
+
+  setTimeout(async () => {
+    const volume = cache.getVolume(volumeId);
+    // Set the volume on the viewport
+    viewport.setVolumes([
+      { volumeId, callback: setCtTransferFunctionForVolumeActor },
+    ]);
+
+    console.log('And now rendering');
+    // Render the image
+    viewport.render();
+  }, 100);
+}
+
+async function loadImage16Float() {
+  console.log('Loading image 16 float');
+  renderingEngine.disableElement(viewportId);
+
+  const viewportInput = {
+    viewportId,
+    type: ViewportType.ORTHOGRAPHIC,
+    element,
+    defaultOptions: {
+      orientation: Enums.OrientationAxis.SAGITTAL,
+      background: <Types.Point3>[0, 0, 0.2],
+    },
+  };
+  renderingEngine.enableElement(viewportInput);
+
+  // Get the volume viewport that was created
+  viewport = <Types.IVolumeViewport>renderingEngine.getViewport(viewportId);
+
+  setTimeout(async () => {
+    cache.setMaxCacheSize(LargeCacheSize);
+    const currentConfig = getConfiguration();
+    setConfiguration({
+      ...currentConfig,
+      rendering: {
+        ...currentConfig.rendering,
+        useNorm16Texture: true,
+      },
+    });
+
+    const volume =
+      cache.getVolume(volumeId16) ||
+      (await volumeLoader.createAndCacheVolume(volumeId16, {
+        imageIds,
+      }));
+
+    // Set the volume to load
+    volume.load();
+
+    // Set the volume on the viewport
+    viewport.setVolumes([
+      { volumeId: volumeId16, callback: setCtTransferFunctionForVolumeActor },
+    ]);
+
+    // Render the image
+    viewport.render();
+  }, 100);
+}
 
 /**
  * Runs the demo
@@ -43,18 +150,16 @@ async function run() {
 
   // Get Cornerstone imageIds and fetch metadata into RAM
   // TODO - move the study into a shared context.
-  const imageIds = await createImageIdsAndCacheMetaData({
+  imageIds = await createImageIdsAndCacheMetaData({
     StudyInstanceUID: '1.2.250.1.90.4.3706890026.20240517115154.2024.1',
     SeriesInstanceUID: '1.2.250.1.90.3.3384839960.20240517203917.8108.53271',
     wadoRsRoot: 'http://localhost:5000/dicomweb',
   });
 
   // Instantiate a rendering engine
-  const renderingEngineId = 'myRenderingEngine';
-  const renderingEngine = new RenderingEngine(renderingEngineId);
+  renderingEngine = new RenderingEngine(renderingEngineId);
 
-  // Create a stack viewport
-  const viewportId = 'CT_SAGITTAL_STACK';
+  // Create a volume viewport
   const viewportInput = {
     viewportId,
     type: ViewportType.ORTHOGRAPHIC,
@@ -68,14 +173,7 @@ async function run() {
   renderingEngine.enableElement(viewportInput);
 
   // Get the stack viewport that was created
-  const viewport = <Types.IVolumeViewport>(
-    renderingEngine.getViewport(viewportId)
-  );
-
-  // Define a unique id for the volume
-  const volumeName = 'CT_VOLUME_ID'; // Id of the volume less loader prefix
-  const volumeLoaderScheme = 'cornerstoneStreamingImageVolume'; // Loader id which defines which volume loader to use
-  const volumeId = `${volumeLoaderScheme}:${volumeName}`; // VolumeId with loader id + volume id
+  viewport = <Types.IVolumeViewport>renderingEngine.getViewport(viewportId);
 
   // Define a volume in memory
   const volume = await volumeLoader.createAndCacheVolume(volumeId, {
