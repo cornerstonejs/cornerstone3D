a60d973ad0860b7ebdaeb6bc4e7423b27e6147c0
Alireza <ar.sedghi@gmail.com>
2024-05-21 17:14:06 -0400

    fix: correct typo in color space conversion function

--- packages/dicomImageLoader/src/imageLoader/isColorConversionRequirementsFulfilled.ts
+++ packages/dicomImageLoader/src/imageLoader/isColorConversionRequirementsFulfilled.ts
@@ -1,8 +1,8 @@
 /**
  * This function checks color space conversion data requirements before
- * apply them. This function was created to solve problems like the one
+ * applying them. This function was created to solve problems like the one
  * discussed in here https://discourse.orthanc-server.org/t/orthanc-convert-ybr-to-rgb-but-does-not-change-metadata/3533/17
- * In this case, Orthanc server convertes the pixel data from YBR to RGB, but maintain
+ * In this case, Orthanc server converts the pixel data from YBR to RGB, but maintain
  * the photometricInterpretation dicom tag in YBR
  * @param imageFrame
  * @param RGBA
