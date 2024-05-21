3083616d5c16021daf7e58e8ae028af92cb2d0f7
Alireza <ar.sedghi@gmail.com>
2024-05-21 09:48:41 -0400

    chore: Update volumeLarge example with lazy loading and cache configuration

--- packages/core/examples/volumeLarge/index.ts
+++ packages/core/examples/volumeLarge/index.ts
@@ -3,6 +3,7 @@ import {
   Types,
   Enums,
   volumeLoader,
+  cache,
   CONSTANTS,
 } from '@cornerstonejs/core';
 import {
@@ -43,18 +44,18 @@ async function run() {
 
   // Get Cornerstone imageIds and fetch metadata into RAM
   // TODO - move the study into a shared context.
-  const imageIds = await createImageIdsAndCacheMetaData({
-    StudyInstanceUID: '1.2.250.1.90.4.3706890026.20240517115154.2024.1',
-    SeriesInstanceUID: '1.2.250.1.90.3.3384839960.20240517203917.8108.53271',
-    wadoRsRoot: 'http://localhost:5000/dicomweb',
-  });
+  // const imageIds = await createImageIdsAndCacheMetaData({
+  //   StudyInstanceUID: '1.2.250.1.90.4.3706890026.20240517115154.2024.1',
+  //   SeriesInstanceUID: '1.2.250.1.90.3.3384839960.20240517203917.8108.53271',
+  //   wadoRsRoot: 'http://localhost:5000/dicomweb',
+  // });
 
   // Instantiate a rendering engine
   const renderingEngineId = 'myRenderingEngine';
   const renderingEngine = new RenderingEngine(renderingEngineId);
 
   // Create a stack viewport
-  const viewportId = 'CT_SAGITTAL_STACK';
+  const viewportId = 'CT_SAGITTAL';
   const viewportInput = {
     viewportId,
     type: ViewportType.ORTHOGRAPHIC,
@@ -73,17 +74,39 @@ async function run() {
   );
 
   // Define a unique id for the volume
-  const volumeName = 'CT_VOLUME_ID'; // Id of the volume less loader prefix
-  const volumeLoaderScheme = 'cornerstoneStreamingImageVolume'; // Loader id which defines which volume loader to use
-  const volumeId = `${volumeLoaderScheme}:${volumeName}`; // VolumeId with loader id + volume id
+  const volumeId = 'CT_VOLUME_ID'; // Id of the volume less loader prefix
+  // const volumeLoaderScheme = 'cornerstoneStreamingImageVolume'; // Loader id which defines which volume loader to use
+  // const volumeId = `${volumeLoaderScheme}:${volumeName}`; // VolumeId with loader id + volume id
 
   // Define a volume in memory
-  const volume = await volumeLoader.createAndCacheVolume(volumeId, {
-    imageIds,
-  });
+
+  // 5 GB
+  cache.setMaxCacheSize(5294967296);
+
+  const dimensions = [750, 750, 1024] as Types.Point3;
+
+  await volumeLoader.createLocalVolume(
+    {
+      dimensions,
+      direction: [1, 0, 0, 0, 1, 0, 0, 0, 1],
+      spacing: [1, 1, 1],
+      origin: [0, 0, 0],
+      // @ts-ignore
+      metadata: {
+        Columns: dimensions[0],
+        Rows: dimensions[1],
+        Modality: 'CT',
+      },
+      targetBuffer: {
+        type: 'Float32Array',
+        sharedArrayBuffer: true,
+      },
+    },
+    volumeId
+  );
 
   // Set the volume to load
-  volume.load();
+  // volume.load();
 
   // Set the volume on the viewport
   viewport.setVolumes([
