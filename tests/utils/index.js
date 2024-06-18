7eef0d2be5160c8fccfc9e324720bcf098f0e4fe
IbrahimCSAE <ibrahim.mdev@gmail.com>
2024-06-17 23:21:54 -0400

    imports

--- tests/utils/index.js
+++ tests/utils/index.js
@@ -2,5 +2,12 @@ import { visitExample } from './visitExample';
 import { checkForScreenshot } from './checkForScreenshot';
 import { screenShotPaths } from './screenShotPaths';
 import { simulateDrag } from './simulateDrag';
+import { simulateClicksOnElement } from './simulateClicksOnElement';
 
-export { visitExample, checkForScreenshot, screenShotPaths, simulateDrag };
+export {
+  visitExample,
+  checkForScreenshot,
+  screenShotPaths,
+  simulateDrag,
+  simulateClicksOnElement,
+};
