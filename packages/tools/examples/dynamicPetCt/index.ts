31d02ac19ba8c741383844bf5c5013ec8d6cb95b
cw <1096486917@qq.com>
2024-07-05 14:04:56 +0800

    fix: StudyInstanceUID data

--- packages/tools/examples/dynamicPetCt/index.ts
+++ packages/tools/examples/dynamicPetCt/index.ts
@@ -529,22 +529,22 @@ function setUpSynchronizers() {
 
 async function setUpDisplay() {
   const { metaDataManager } = cornerstoneDICOMImageLoader.wadors;
-  const wadoRsRoot = 'https://d33do7qe4w26qo.cloudfront.net/dicomweb';
+  const wadoRsRoot = 'https://d3t6nz73ql33tx.cloudfront.net/dicomweb';
   const StudyInstanceUID =
-    '1.3.6.1.4.1.12842.1.1.14.3.20220915.105557.468.2963630849';
+    '1.3.6.1.4.1.14519.5.2.1.7009.2403.334240657131972136850343327463';
 
   // Get Cornerstone imageIds and fetch metadata into RAM
   let ctImageIds = await createImageIdsAndCacheMetaData({
     StudyInstanceUID,
     SeriesInstanceUID:
-      '1.3.6.1.4.1.12842.1.1.14.4.20220915.121025.435.2500855592',
+      '1.3.6.1.4.1.14519.5.2.1.7009.2403.226151125820845824875394858561',
     wadoRsRoot,
   });
 
   const ptImageIds = await createImageIdsAndCacheMetaData({
     StudyInstanceUID,
     SeriesInstanceUID:
-      '1.3.6.1.4.1.12842.1.1.22.4.20220915.124758.560.4125514885',
+      '1.3.6.1.4.1.14519.5.2.1.7009.2403.879445243400782656317561081015',
     wadoRsRoot,
   });
 
