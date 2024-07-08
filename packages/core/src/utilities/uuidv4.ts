49796d0ff7d84f70716ea81d3b621dbae02ad936
Celian-abd <101793092+Celian-abd@users.noreply.github.com>
2024-07-08 10:46:15 +0200

    revert

--- packages/core/src/utilities/uuidv4.ts
+++ packages/core/src/utilities/uuidv4.ts
@@ -6,13 +6,7 @@
  */
 export default function uuidv4(): string {
   // @ts-ignore
-  return (
-    crypto.randomUUID?.() ||
-    ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
-      (
-        c ^
-        (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
-      ).toString(16)
-    )
+  return crypto.randomUUID?.() || ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
   );
 }
