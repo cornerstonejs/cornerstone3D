bc85ec1b91dfc4d9f6f6770e40b21a5548f406e0
Lisa Müllner <lisa.muellner@dedalus.com>
2024-08-01 16:15:51 +0200

    DVISA-1673: use cornerstone internal metadatamodules enum if external not available

--- packages/dicomImageLoader/src/imageLoader/legacyMetadataModules.ts
+++
@@ -1,7 +0,0 @@
-export enum LegacyMetadataModules {
-  IMAGE_PLANE = 'imagePlaneModule',
-  VOI_LUT = 'voiLutModule',
-  MODALITY_LUT = 'modalityLutModule',
-  SOP_COMMON = 'sopCommonModule',
-  CALIBRATION = '',
-}
