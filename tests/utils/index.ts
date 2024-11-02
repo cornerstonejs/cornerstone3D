19f2aec9fe7ba9f4492c5ff2a1c36af33566e186
Ibrahim <ibrahim.mdev@gmail.com>
2024-11-02 09:38:35 -0400

    updates

--- tests/utils/index.ts
+++ tests/utils/index.ts
@@ -3,4 +3,3 @@ export { checkForScreenshot } from './checkForScreenshot';
 export { screenShotPaths } from './screenShotPaths';
 export { simulateDrag } from './simulateDrag';
 export { simulateClicksOnElement } from './simulateClicksOnElement';
-export { simulateDrawPath } from './simulateDrawPath';
--- tests/utils/locatorToPageCoord.ts
+++
@@ -1,7 +0,0 @@
-import locatorToPageCoords from './locatorToPageCoords';
-
-export default async function locatorToPageCoord(locator, point: number[]) {
-  const pagePoints = await locatorToPageCoords(locator, [point]);
-
-  return pagePoints[0];
-}
