c1c89366669d94ba4bd0bc480c593e2dbf1494c9
Ibrahim <ibrahim.mdev@gmail.com>
2024-11-08 08:25:30 -0500

    remove test

--- tests/surfaceRendering.spec.ts
+++ tests/surfaceRendering.spec.ts
@@ -3,18 +3,16 @@ import {
   visitExample,
   checkForScreenshot,
   screenShotPaths,
-  reduceViewportsSize,
-  attemptAction,
 } from './utils/index';
 
 test.beforeEach(async ({ page }) => {
-  await visitExample(page, 'surfaceRendering', 0, false, false);
+  await visitExample(page, 'surfaceRendering');
 });
 
-test.describe('Surface Segmentation Representation for Volume Viewports', async () => {
+// this is too much for the CI.
+test.skip('Surface Segmentation Representation for Volume Viewports', async () => {
   test('should render the segmentation correctly', async ({ page }) => {
     const locator = page.locator('.cornerstone-canvas');
-    await attemptAction(() => reduceViewportsSize(page), 1000, 10);
     await checkForScreenshot(
       page,
       locator,
