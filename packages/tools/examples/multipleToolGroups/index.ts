781f9a6876d06b990871b9afc7638eac33cfc064
Alireza <ar.sedghi@gmail.com>
2024-11-26 16:52:35 -0500

    chore: update server (#1625)

--- packages/tools/examples/multipleToolGroups/index.ts
+++ packages/tools/examples/multipleToolGroups/index.ts
@@ -117,7 +117,7 @@ async function run() {
     bindings: [{ mouseButton: MouseBindings.Wheel }],
   });
 
-  const wadoRsRoot = 'https://d3t6nz73ql33tx.cloudfront.net/dicomweb';
+  const wadoRsRoot = 'https://d14fa38qiwhyfd.cloudfront.net/dicomweb';
   const StudyInstanceUID =
     '1.3.6.1.4.1.14519.5.2.1.7009.2403.334240657131972136850343327463';
 
