9f822cbb1ae5dc14b9238b044feedef603c55ae5
sedghi <ar.sedghi@gmail.com>
2024-11-26 15:36:40 -0500

    Documentation and Tools: Update linking guide and refactor annotation tool UI
    -  Remove redundant URL encoding in the YouTube iframe src attribute in linking.md
    -  Refactor dynamicallyAddAnnotations example by removing hardcoded forms and event listeners
    -  Introduce a new toolSpecificUI module to dynamically generate UI forms based on the selected tool
    -  Enhance LengthTool with a static

--- packages/docs/docs/contribute/linking.md
+++ packages/docs/docs/contribute/linking.md
@@ -108,7 +108,7 @@ If changes aren't reflecting:
 
 ## Video Tutorials
 
-<iframe width="560" height="315" src="[https://www.youtube.com/embed/IOXQ1od6DZA?si=3QP4rppQgedJn7y8](https://www.youtube.com/embed/IOXQ1od6DZA?si=3QP4rppQgedJn7y8)" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/IOXQ1od6DZA?si=3QP4rppQgedJn7y8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
 ## Tips
 
---
+++ packages/tools/examples/dynamicallyAddAnnotations/toolSpecificUI.ts
@@ -0,0 +1,220 @@
+import {
+  getEnabledElementByViewportId,
+  getRenderingEngine,
+  utilities,
+} from '@cornerstonejs/core';
+import type { Point3 } from '@cornerstonejs/core/types/Point3';
+import type { Point2 } from '@cornerstonejs/core/types';
+import { LengthTool } from '@cornerstonejs/tools';
+
+interface ToolUIConfig {
+  toolName: string;
+  renderingEngineId: string;
+  content: HTMLElement;
+  demoToolbar: HTMLElement;
+}
+
+interface ToolUI {
+  forms: HTMLElement[];
+}
+
+export const STACK_VIEWPORT_ID = 'viewport-stack';
+export const VOLUME_VIEWPORT_ID = 'viewport-volume';
+
+const typeToIdMap = {
+  stack: STACK_VIEWPORT_ID,
+  volume: VOLUME_VIEWPORT_ID,
+};
+
+const typeToStartIdMap = {
+  canvas: 'canvas-start',
+  image: 'image-start',
+};
+
+const typeToEndIdMap = {
+  canvas: 'canvas-end',
+  image: 'image-end',
+};
+
+function getViewport(viewportType: string) {
+  const enabledElement = getEnabledElementByViewportId(
+    typeToIdMap[viewportType]
+  );
+  return enabledElement.viewport;
+}
+
+/**
+ * Creates and returns the UI elements for the Length tool
+ */
+function createLengthToolUI(config: ToolUIConfig): ToolUI {
+  const { content, demoToolbar } = config;
+
+  // Add input elements for Image Coords
+  const canvasCoordsForm = document.createElement('form');
+  canvasCoordsForm.style.marginTop = '20px';
+  canvasCoordsForm.style.marginBottom = '10px';
+  canvasCoordsForm.innerHTML = `
+  <label style="margin-right: 20px;">Canvas Coords: Start [x, y]:</label>
+  <input  style="width:40px " type="number" id="canvas-start-1" placeholder="Start x" value="0">
+  <input  style="width:40px" type="number" id="canvas-start-2" placeholder="Start y" value="0">
+  <label style="margin-left: 52px; margin-right: 21px;">End [x, y]:</label>
+  <input  style="width:40px" type="number" id="canvas-end-1" placeholder="End i" value="100">
+  <input  style="width:40px" type="number" id="canvas-end-2" placeholder="End j" value="100">
+  <button  style="  margin-left: 52px;" type="button" id="canvas-stack">Add Stack</button>
+  <button   type="button" id="canvas-volume">Add Volume</button>
+`;
+  const imageCoordsForm = document.createElement('form');
+  imageCoordsForm.style.marginBottom = '10px';
+  imageCoordsForm.innerHTML = `
+  <label style="margin-right: 20px;">Image Coords: Start [i, j]:</label>
+  <input  style="width:40px " type="number" id="image-start-1" placeholder="Start i" value="0">
+  <input  style="width:40px" type="number" id="image-start-2" placeholder="Start j" value="0">
+  <label style="margin-left: 52px; margin-right: 21px;">End [i, j]:</label>
+  <input  style="width:40px" type="number" id="image-end-1" placeholder="End i" value="100">
+  <input  style="width:40px" type="number" id="image-end-2" placeholder="End j" value="100">
+  <button  style="  margin-left: 52px;" type="button" id="image-stack">Add Stack</button>
+  <button   type="button" id="image-volume">Add Volume</button>
+`;
+
+  // add event listeners to all buttons
+  const buttons = [
+    canvasCoordsForm.querySelector('#canvas-stack'),
+    canvasCoordsForm.querySelector('#canvas-volume'),
+    imageCoordsForm.querySelector('#image-stack'),
+    imageCoordsForm.querySelector('#image-volume'),
+  ];
+
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      // which target was clicked?
+      const type = button.id.split('-')[0];
+      const viewportType = button.id.split('-')[1];
+
+      let start: Point2 | Point3;
+      let end: Point2 | Point3;
+
+      const viewport = getViewport(viewportType);
+
+      // get the start and end points from the form
+      let worldStart: Point3;
+      let worldEnd: Point3;
+
+      if (type === 'image') {
+        const start = [
+          Number(
+            imageCoordsForm.querySelector(`#${typeToStartIdMap.image}-1`).value
+          ),
+          Number(
+            imageCoordsForm.querySelector(`#${typeToStartIdMap.image}-2`).value
+          ),
+        ];
+        const end = [
+          Number(
+            imageCoordsForm.querySelector(`#${typeToEndIdMap.image}-1`).value
+          ),
+          Number(
+            imageCoordsForm.querySelector(`#${typeToEndIdMap.image}-2`).value
+          ),
+        ];
+
+        worldStart = utilities.imageToWorldCoords(
+          viewport.getCurrentImageId(),
+          [...start]
+        );
+        worldEnd = utilities.imageToWorldCoords(viewport.getCurrentImageId(), [
+          ...end,
+        ]);
+      } else if (type === 'canvas') {
+        const start = [
+          Number(
+            canvasCoordsForm.querySelector(`#${typeToStartIdMap.canvas}-1`)
+              .value
+          ),
+          Number(
+            canvasCoordsForm.querySelector(`#${typeToStartIdMap.canvas}-2`)
+              .value
+          ),
+        ];
+        const end = [
+          Number(
+            canvasCoordsForm.querySelector(`#${typeToEndIdMap.canvas}-1`).value
+          ),
+          Number(
+            canvasCoordsForm.querySelector(`#${typeToEndIdMap.canvas}-2`).value
+          ),
+        ];
+
+        worldStart = viewport.canvasToWorld(start);
+        worldEnd = viewport.canvasToWorld(end);
+      }
+
+      LengthTool.hydrate(viewport.id, [worldStart, worldEnd]);
+    });
+  });
+
+  return {
+    forms: [canvasCoordsForm, imageCoordsForm],
+    // cleanup,
+  };
+}
+
+/**
+ * Creates and returns the UI elements for the Probe tool
+ */
+function createProbeToolUI(config: ToolUIConfig): ToolUI {
+  // Add input elements for Canvas Coords
+  const canvasCoordsForm = document.createElement('form');
+  canvasCoordsForm.style.marginBottom = '10px';
+  canvasCoordsForm.innerHTML = `
+  <label style="margin-right: 20px;">Canvas Coords: Point [x, y]:</label>
+  <input style="width:40px" type="number" id="point-x" placeholder="x" value="0">
+  <input style="width:40px" type="number" id="point-y" placeholder="y" value="0">
+  <button style="margin-left: 52px;" type="button" id="add-canvas-coords-stack">Add Stack</button>
+  <button type="button" id="add-canvas-coords-volume">Add Volume</button>
+`;
+
+  // Add input elements for Image Coords
+  const imageCoordsForm = document.createElement('form');
+  imageCoordsForm.style.marginBottom = '10px';
+  imageCoordsForm.innerHTML = `
+  <label style="margin-right: 20px;">Image Coords: Point [i, j]:</label>
+  <input style="width:40px" type="number" id="point-i" placeholder="i" value="0">
+  <input style="width:40px" type="number" id="point-j" placeholder="j" value="0">
+  <button style="margin-left: 52px;" type="button" id="add-image-coords-stack">Add Stack</button>
+  <button type="button" id="add-image-coords-volume">Add Volume</button>
+`;
+
+  // Add input elements for World Coords
+  const worldCoordsForm = document.createElement('form');
+  worldCoordsForm.style.marginBottom = '10px';
+  worldCoordsForm.innerHTML = `
+  <label>World Coords: Point [x, y, z]:</label>
+  <input style="width:40px" type="number" id="point-x" placeholder="x" value="0">
+  <input style="width:40px" type="number" id="point-y" placeholder="y" value="0">
+  <input style="width:40px" type="number" id="point-z" placeholder="z" value="0">
+  <button style="margin-left: 52px;" type="button" id="add-world-coords-stack">Add Stack</button>
+  <button type="button" id="add-world-coords-volume">Add Volume</button>
+`;
+
+  return {
+    forms: [canvasCoordsForm, imageCoordsForm, worldCoordsForm],
+    cleanup: () => {},
+  };
+}
+
+const toolUIRegistry = {
+  Length: createLengthToolUI,
+  Probe: createProbeToolUI,
+};
+
+export function createToolUI(
+  toolName: string,
+  config: ToolUIConfig
+): ToolUI | null {
+  const creator = toolUIRegistry[toolName];
+  if (!creator) {
+    console.debug('No UI configuration for tool:', toolName);
+    return null;
+  }
+  return creator(config);
+}
