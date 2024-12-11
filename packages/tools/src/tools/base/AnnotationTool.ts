22a899c7162b0c0cc8e44dfaa52acdf222c3aca8
alex-stout <17804548+alex-stout@users.noreply.github.com>
2024-12-11 15:21:30 -0600

    docs(AnnotationTool): update JSDoc comments

--- packages/tools/src/tools/base/AnnotationTool.ts
+++ packages/tools/src/tools/base/AnnotationTool.ts
@@ -116,6 +116,7 @@ abstract class AnnotationTool extends AnnotationDisplayTool {
   }
 
   static toolName;
+
   // ===================================================================
   // Abstract Methods - Must be implemented.
   // ===================================================================
@@ -147,11 +148,12 @@ abstract class AnnotationTool extends AnnotationDisplayTool {
   /**
    * @abstract cancel Used to cancel the ongoing tool drawing and manipulation
    *
+   * @param element - The HTML element
    */
-  abstract cancel(element: HTMLDivElement);
+  abstract cancel(element: HTMLDivElement): void;
 
   /**
-   * handleSelectedCallback Custom callback for when a handle is selected.
+   * @abstract handleSelectedCallback Custom callback for when a handle is selected.
    *
    * @param evt - The normalized mouse event
    * @param annotation - The annotation selected.
@@ -166,7 +168,7 @@ abstract class AnnotationTool extends AnnotationDisplayTool {
   ): void;
 
   /**
-   * Custom callback for when an annotation is selected
+   * @abstract toolSelectedCallback Custom callback for when an annotation is selected
    *
    * @param evt - The normalized mouse event
    * @param annotation - The `Annotation` to check.
@@ -180,7 +182,7 @@ abstract class AnnotationTool extends AnnotationDisplayTool {
   ): void;
 
   /**
-   * Returns true if the provided canvas coordinate tool is near the annotation
+   * @abstract isPointNearTool Returns true if the provided canvas coordinate tool is near the annotation
    *
    * @param element - The HTML element
    * @param annotation - The annotation to check
@@ -200,7 +202,6 @@ abstract class AnnotationTool extends AnnotationDisplayTool {
 
   /**
    * @virtual Event handler for Cornerstone MOUSE_MOVE event.
-   *
    *
    * @param evt - The normalized mouse event
    * @param filteredAnnotations - The annotations to check for hover interactions
