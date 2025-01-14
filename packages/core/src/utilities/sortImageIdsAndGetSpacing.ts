033092ddc7d0a1b33d4e442d3897e8d421b7b089
sedghi <ar.sedghi@gmail.com>
2025-01-14 18:01:37 -0500

    fix(sortImageIdsAndGetSpacing): ensure zSpacing is always positive by using Math.abs for sliceThickness and spacingBetweenSlices

--- packages/core/src/utilities/sortImageIdsAndGetSpacing.ts
+++ packages/core/src/utilities/sortImageIdsAndGetSpacing.ts
@@ -157,14 +157,14 @@ export default function sortImageIdsAndGetSpacing(
   // If possible, we use the sliceThickness, but we warn about this dicom file
   // weirdness. If sliceThickness is not available, we set to 1 just to render
   if (zSpacing === 0 && !strictZSpacingForVolumeViewport) {
-    if (sliceThickness && spacingBetweenSlices) {
+    if (!sliceThickness && spacingBetweenSlices) {
       console.log('Could not calculate zSpacing. Using spacingBetweenSlices');
-      zSpacing = spacingBetweenSlices;
+      zSpacing = Math.abs(spacingBetweenSlices);
     } else if (sliceThickness) {
       console.log(
         'Could not calculate zSpacing and no spacingBetweenSlices. Using sliceThickness'
       );
-      zSpacing = sliceThickness;
+      zSpacing = Math.abs(sliceThickness);
     } else {
       console.log(
         'Could not calculate zSpacing. The VolumeViewport visualization is compromised. Setting zSpacing to 1 to render'
