2d24c517960a3f192304102e7d82dad2009510dc
Ibrahim <93064150+IbrahimCSAE@users.noreply.github.com>
2025-03-10 20:36:43 -0400

    updates

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,9 +4,9 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 4 : undefined,
-  timeout: 720 * 1000,
+  retries: process.env.CI ? 1 : 0,
+  workers: process.env.CI ? 1 : 4,
+  timeout: 180 * 1000,
   snapshotPathTemplate:
     'tests/screenshots{/projectName}/{testFilePath}/{arg}{ext}',
   outputDir: './tests/test-results',
