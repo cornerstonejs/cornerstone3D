5ba50d2918c9fac2da9f2edf4addef55691b8573
Martin Bellehumeur, M. Eng. <23396581+mbellehumeur@users.noreply.github.com>
2025-05-29 09:24:22 +0200

    Update RectangleROITool.ts with min value

--- packages/tools/src/tools/annotation/RectangleROITool.ts
+++ packages/tools/src/tools/annotation/RectangleROITool.ts
@@ -1074,6 +1074,9 @@ function defaultGetTextLines(data, targetId: string): string[] {
   if (AnnotationTool.isNumber(max)) {
     textLines.push(`Max: ${csUtils.roundNumber(max)} ${modalityUnit}`);
   }
+  if (AnnotationTool.isNumber(min)) {
+    textLines.push(`Max: ${csUtils.roundNumber(min)} ${modalityUnit}`);
+  }
   if (AnnotationTool.isNumber(stdDev)) {
     textLines.push(`Std Dev: ${csUtils.roundNumber(stdDev)} ${modalityUnit}`);
   }
