675e2cfba6b560e50af790630799057f80492c7c
rodrigobasilio2022 <114958722+rodrigobasilio2022@users.noreply.github.com>
2025-06-09 23:37:17 -0300

    feat: Add simplified spline configuration (#2124)
    * Add simplified spline configuration
    
    * Fix bug in cut merge process
    
    * Fix issues pointed out by reviewer

--- packages/tools/src/tools/annotation/SplineContourSegmentationTool.ts
+++ packages/tools/src/tools/annotation/SplineContourSegmentationTool.ts
@@ -1,9 +1,12 @@
-import { utilities } from '@cornerstonejs/core';
+import { eventTarget, utilities } from '@cornerstonejs/core';
 import type { PublicToolProps } from '../../types';
 import SplineROITool from './SplineROITool';
+import { Events } from '../../enums';
+import { convertContourSegmentationAnnotation } from '../../utilities/contourSegmentation';
 
 class SplineContourSegmentationTool extends SplineROITool {
   static toolName = 'SplineContourSegmentationTool';
+  private annotationCutMergeCompletedBinded;
 
   constructor(toolProps: PublicToolProps) {
     const initialProps = utilities.deepMerge(
@@ -16,11 +19,38 @@ class SplineContourSegmentationTool extends SplineROITool {
     );
 
     super(initialProps);
+    this.annotationCutMergeCompletedBinded =
+      this.annotationCutMergeCompleted.bind(this);
   }
 
   protected isContourSegmentationTool(): boolean {
     // Re-enable contour segmentation behavior disabled by SplineROITool
     return true;
+  }
+
+  protected initializeListeners() {
+    eventTarget.addEventListener(
+      Events.ANNOTATION_CUT_MERGE_PROCESS_COMPLETED,
+      this.annotationCutMergeCompletedBinded
+    );
+  }
+
+  protected removeListeners() {
+    eventTarget.removeEventListener(
+      Events.ANNOTATION_CUT_MERGE_PROCESS_COMPLETED,
+      this.annotationCutMergeCompletedBinded
+    );
+  }
+
+  protected annotationCutMergeCompleted(evt) {
+    const { sourceAnnotation: annotation } = evt.detail;
+    if (
+      !this.splineToolNames.includes(annotation?.metadata?.toolName) ||
+      !this.configuration.simplifiedSpline
+    ) {
+      return;
+    }
+    convertContourSegmentationAnnotation(annotation);
   }
 }
 
