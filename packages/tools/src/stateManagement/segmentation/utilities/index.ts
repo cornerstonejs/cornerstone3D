ac8f8b66a695e320a15aa96a762d9bcee27d434d
rodrigobasilio2022 <114958722+rodrigobasilio2022@users.noreply.github.com>
2025-06-12 17:53:15 -0300

    feat(contour): expose Contour apis for modifications (#2132)

---
+++ packages/tools/src/stateManagement/segmentation/utilities/index.ts
@@ -0,0 +1,11 @@
+export {
+  getViewportAssociatedToSegmentation,
+  getViewportWithMatchingViewPlaneNormal,
+} from './getViewportAssociatedToSegmentation';
+export { getAnnotationMapFromSegmentation } from './getAnnotationMapFromSegmentation';
+export { default as decimateContours } from './decimateContours';
+export { extractSegmentPolylines } from './extractSegmentPolylines';
+export { removeCompleteContourAnnotation } from './removeCompleteContourAnnotation';
+export { default as removeContourHoles } from './removeContourHoles';
+export { default as removeContourIslands } from './removeContourIslands';
+export { default as smoothContours } from './smoothContours';
