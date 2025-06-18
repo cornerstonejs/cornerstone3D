e42a6f63e14584fe6f2c403fca9fc8d7b789d8d3
VSP <vishnusureshperumbavoor@gmail.com>
2025-06-18 21:51:24 +0530

    Updated comments

--- packages/ai/src/enums/Events.ts
+++ packages/ai/src/enums/Events.ts
@@ -3,15 +3,15 @@
  */
 enum Events {
   /**
-   * Triggers when the ONNX model loading starts.
+   * Triggers when the AI model loading starts.
    */
   MODEL_LOADING_STARTED = 'AI_MODEL_LOADING_STARTED',
   /**
-   * Triggers when the ONNX model loading is completed.
+   * Triggers when the AI model loading is completed.
    */
   MODEL_LOADING_COMPLETED = 'AI_MODEL_LOADING_COMPLETED',
   /**
-   * Triggers when a component of the ONNX model is loaded.
+   * Triggers when a component of the AI model is loaded.
    */
   MODEL_COMPONENT_LOADED = 'AI_MODEL_COMPONENT_LOADED',
 }
