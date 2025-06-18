7f28426d6bf6b128b53901a3fcab0fb5d3994090
VSP <vishnusureshperumbavoor@gmail.com>
2025-06-18 21:49:47 +0530

    Changed ONNX generic event name to AI

--- packages/ai/src/enums/Events.ts
+++ packages/ai/src/enums/Events.ts
@@ -5,15 +5,15 @@ enum Events {
   /**
    * Triggers when the ONNX model loading starts.
    */
-  MODEL_LOADING_STARTED = 'ONNX_MODEL_LOADING_STARTED',
+  MODEL_LOADING_STARTED = 'AI_MODEL_LOADING_STARTED',
   /**
    * Triggers when the ONNX model loading is completed.
    */
-  MODEL_LOADING_COMPLETED = 'ONNX_MODEL_LOADING_COMPLETED',
+  MODEL_LOADING_COMPLETED = 'AI_MODEL_LOADING_COMPLETED',
   /**
    * Triggers when a component of the ONNX model is loaded.
    */
-  MODEL_COMPONENT_LOADED = 'ONNX_MODEL_COMPONENT_LOADED',
+  MODEL_COMPONENT_LOADED = 'AI_MODEL_COMPONENT_LOADED',
 }
 
 export default Events;
