0a5058fa54343babc9e73482098d0785125e01ad
Martin Bellehumeur, M. Eng. <23396581+mbellehumeur@users.noreply.github.com>
2025-07-09 23:33:16 +0200

    refactor(volumeViewport3D): Remove unnecessary blank lines in VolumeViewport3D constructor

--- packages/core/src/RenderingEngine/VolumeViewport3D.ts
+++ packages/core/src/RenderingEngine/VolumeViewport3D.ts
@@ -10,6 +10,7 @@ import type vtkVolume from '@kitware/vtk.js/Rendering/Core/Volume';
 import type { ViewportInput } from '../types/IViewport';
 import type { ImageActor } from '../types/IActor';
 import BaseVolumeViewport from './BaseVolumeViewport';
+
 /**
  * An object representing a 3-dimensional volume viewport. VolumeViewport3Ds are used to render
  * 3D volumes in their entirety, and not just load a single slice at a time.
@@ -20,6 +21,7 @@ import BaseVolumeViewport from './BaseVolumeViewport';
 class VolumeViewport3D extends BaseVolumeViewport {
   constructor(props: ViewportInput) {
     super(props);
+
     const { parallelProjection, orientation } = this.options;
 
     const activeCamera = this.getVtkActiveCamera();
