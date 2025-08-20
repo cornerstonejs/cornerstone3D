412cb38b7c0efcb19290f920e4984ddf9a70b029
Martin Bellehumeur, M. Eng. <23396581+mbellehumeur@users.noreply.github.com>
2025-08-20 22:19:26 +0200

    Refactor createVolumeMapper: Simplify sample distance calculation by removing unnecessary conditional check for sampleDistanceMultiplier

--- packages/core/src/RenderingEngine/helpers/createVolumeMapper.ts
+++ packages/core/src/RenderingEngine/helpers/createVolumeMapper.ts
@@ -35,9 +35,8 @@ export default function createVolumeMapper(
   const sampleDistanceMultiplier =
     getConfiguration().rendering?.volumeRendering?.sampleDistanceMultiplier ||
     1;
-  if (sampleDistanceMultiplier !== undefined) {
-    sampleDistance = sampleDistance * sampleDistanceMultiplier;
-  }
+
+  sampleDistance = sampleDistance * sampleDistanceMultiplier;
 
   // This is to allow for good pixel level image quality.
   // Todo: why we are setting this to 4000? Is this a good number? it should be configurable
