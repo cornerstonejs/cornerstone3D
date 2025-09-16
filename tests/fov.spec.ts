2a52f039de744386d7fc687aa7a54b0f9222c653
Ibrahim <ibrahim.mdev@gmail.com>
2025-09-16 18:00:15 -0400

    updates

--- tests/fov.spec.ts
+++ tests/fov.spec.ts
@@ -13,10 +13,9 @@ test.describe('Field of View (FOV)', async () => {
   test('should display the correct FOV for each viewport', async ({
     page,
   }) => {
-    // Set custom viewport size for this test
     await page.setViewportSize({ width: 1920, height: 1080 });
 
-    await page.waitForTimeout(3000);
+
     await checkForScreenshot(
       page,
       page,
