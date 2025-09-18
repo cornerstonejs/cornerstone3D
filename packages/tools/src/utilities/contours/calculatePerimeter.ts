1075d002a3d0168ba2ef1702e7609486448bac7f
Arul Mozhi J <arul.mozhi@trenser.com>
2025-09-18 17:57:16 +0530

    fix: planar freehand roi perimeter calculation

--- packages/tools/src/utilities/contours/calculatePerimeter.ts
+++ packages/tools/src/utilities/contours/calculatePerimeter.ts
@@ -1,3 +1,5 @@
+import type { Types } from '@cornerstonejs/core';
+
 /**
  * Calculates the perimeter of a polyline.
  *
@@ -9,23 +11,29 @@ function calculatePerimeter(polyline: number[][], closed: boolean): number {
   let perimeter = 0;
 
   for (let i = 0; i < polyline.length - 1; i++) {
-    const point1 = polyline[i];
-    const point2 = polyline[i + 1];
-    perimeter += Math.sqrt(
-      Math.pow(point2[0] - point1[0], 2) + Math.pow(point2[1] - point1[1], 2)
-    );
+    const point1 = polyline[i] as Types.Point3;
+    const point2 = polyline[i + 1] as Types.Point3;
+
+    perimeter += _calculatePerimeter(point1, point2);
   }
 
   if (closed) {
-    const firstPoint = polyline[0];
-    const lastPoint = polyline[polyline.length - 1];
-    perimeter += Math.sqrt(
-      Math.pow(lastPoint[0] - firstPoint[0], 2) +
-        Math.pow(lastPoint[1] - firstPoint[1], 2)
-    );
+    const firstPoint = polyline[0] as Types.Point3;
+    const lastPoint = polyline[polyline.length - 1] as Types.Point3;
+    perimeter += _calculatePerimeter(firstPoint, lastPoint);
   }
 
   return perimeter;
+}
+
+function _calculatePerimeter(
+  point1: Types.Point3,
+  point2: Types.Point3
+): number {
+  const dx = point1[0] - point2[0];
+  const dy = point1[1] - point2[1];
+  const dz = point1[2] - point2[2];
+  return Math.sqrt(dx * dx + dy * dy + dz * dz);
 }
 
 export default calculatePerimeter;
