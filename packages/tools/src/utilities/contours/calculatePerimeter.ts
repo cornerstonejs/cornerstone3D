233e99fac6389457517aaabc504fcd3657cd210d
Bill Wallace <wayfarer3130@gmail.com>
2025-10-17 15:54:07 -0400

    fix: Perimeter calculation

--- packages/tools/src/utilities/contours/calculatePerimeter.ts
+++ packages/tools/src/utilities/contours/calculatePerimeter.ts
@@ -1,4 +1,5 @@
 import type { Types } from '@cornerstonejs/core';
+import { vec3 } from 'gl-matrix';
 
 /**
  * Calculates the perimeter of a polyline.
@@ -7,33 +8,26 @@ import type { Types } from '@cornerstonejs/core';
  * @param closed - Indicates whether the polyline is closed or not.
  * @returns The perimeter of the polyline.
  */
-function calculatePerimeter(polyline: number[][], closed: boolean): number {
+export function calculatePerimeter(
+  polyline: number[][],
+  closed: boolean
+): number {
   let perimeter = 0;
 
   for (let i = 0; i < polyline.length - 1; i++) {
     const point1 = polyline[i] as Types.Point3;
     const point2 = polyline[i + 1] as Types.Point3;
 
-    perimeter += _calculatePerimeter(point1, point2);
+    perimeter += vec3.dist(point1, point2);
   }
 
   if (closed) {
     const firstPoint = polyline[0] as Types.Point3;
     const lastPoint = polyline[polyline.length - 1] as Types.Point3;
-    perimeter += _calculatePerimeter(firstPoint, lastPoint);
+    perimeter += vec3.dist(firstPoint, lastPoint);
   }
 
   return perimeter;
-}
-
-function _calculatePerimeter(
-  point1: Types.Point3,
-  point2: Types.Point3
-): number {
-  const dx = point1[0] - point2[0];
-  const dy = point1[1] - point2[1];
-  const dz = point1[2] - point2[2];
-  return Math.sqrt(dx * dx + dy * dy + dz * dz);
 }
 
 export default calculatePerimeter;
