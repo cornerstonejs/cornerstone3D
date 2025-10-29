d94328cc3ed81995d665dcd1736db98c00e3978b
Ibrahim <93064150+IbrahimCSAE@users.noreply.github.com>
2025-10-29 15:56:59 -0400

    feat(exports): add intializers exports to dicom image loader (#2432)
    * ss
    
    * ss

---
+++ packages/dicomImageLoader/src/shared/decoders/index.ts
@@ -0,0 +1,17 @@
+import { initialize as initializeHTJ2K } from './decodeHTJ2K';
+import { initialize as initializeJPEG2000 } from './decodeJPEG2000';
+import { initialize as initializeJPEGLS } from './decodeJPEGLS';
+import { initialize as initializeJPEGBaseline12Bit } from './decodeJPEGBaseline12Bit-js';
+import { initialize as initializeJPEGLossless } from './decodeJPEGLossless';
+import { initialize as initLibjpegTurbo } from './decodeJPEGBaseline8Bit';
+
+const initializers = {
+  HTJ2K: initializeHTJ2K,
+  JPEG2000: initializeJPEG2000,
+  JPEGLS: initializeJPEGLS,
+  JPEGBaseline12Bit: initializeJPEGBaseline12Bit,
+  JPEGLossless: initializeJPEGLossless,
+  JPEGBaseline8Bit: initLibjpegTurbo,
+};
+
+export { initializers };
