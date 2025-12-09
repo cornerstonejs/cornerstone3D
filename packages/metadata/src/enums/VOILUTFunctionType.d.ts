1f13aa5ec82887220cb84bc71138bbe4461b8225
Bill Wallace <wayfarer3130@gmail.com>
2025-12-09 17:27:21 -0500

    Remove incorrect adds of prebuilt files

--- packages/metadata/src/enums/VOILUTFunctionType.d.ts
+++
@@ -1,6 +0,0 @@
-export declare enum VoiLutFunctionType {
-  LINEAR = 'LINEAR',
-  SAMPLED_SIGMOID = 'SIGMOID',
-  LINEAR_EXACT = 'LINEAR_EXACT',
-}
-export default VoiLutFunctionType;
--- packages/metadata/src/enums/VOILUTFunctionType.js
+++
@@ -1,7 +0,0 @@
-export var VoiLutFunctionType;
-(function (VoiLutFunctionType) {
-  VoiLutFunctionType['LINEAR'] = 'LINEAR';
-  VoiLutFunctionType['SAMPLED_SIGMOID'] = 'SIGMOID';
-  VoiLutFunctionType['LINEAR_EXACT'] = 'LINEAR_EXACT';
-})(VoiLutFunctionType || (VoiLutFunctionType = {}));
-export default VoiLutFunctionType;
