1caed7e88b01ae61e9f48334e481cd009c60c42f
Bill Wallace <wayfarer3130@gmail.com>
2026-02-23 12:15:13 -0500

    Revert changes to adapters

--- packages/adapters/src/adapters/Cornerstone3D/LabelData.ts
+++ packages/adapters/src/adapters/Cornerstone3D/LabelData.ts
@@ -1,11 +1,11 @@
-import type { Types } from '@cornerstonejs/tools';
-import { COMMENT_CODE, TEXT_ANNOTATION_POSITION } from './constants';
-import { toScoord } from '../helpers';
-import dcmjs from 'dcmjs';
+import type { Types } from "@cornerstonejs/tools";
+import { COMMENT_CODE, TEXT_ANNOTATION_POSITION } from "./constants";
+import { toScoord } from "../helpers";
+import dcmjs from "dcmjs";
 
 const {
-  sr: { valueTypes, coding },
-  adapters: { Cornerstone3D },
+    sr: { valueTypes, coding },
+    adapters: { Cornerstone3D }
 } = dcmjs;
 
 /**
@@ -17,123 +17,125 @@ const {
  * tagged to a private text annotation position value.
  */
 export default class LabelData {
-  protected tid300Item;
-  protected annotation: Types.Annotation;
-  public ReferencedSOPSequence;
+    protected tid300Item;
+    protected annotation: Types.Annotation;
+    public ReferencedSOPSequence;
 
-  constructor(tid300Item, annotation: Types.Annotation) {
-    this.tid300Item = tid300Item;
-    this.annotation = annotation;
-    this.ReferencedSOPSequence = tid300Item.ReferencedSOPSequence;
-  }
-
-  /**
-   * This adds the additional information from the annotation data for
-   * the label data.
-   */
-  public contentItem() {
-    const contentEntries = this.tid300Item.contentItem();
-    const { label, handles } = this.annotation.data;
-
-    if (label) {
-      contentEntries.push(this.createQualitativeLabel(label));
-      this.filterCornerstoneFreeText(contentEntries);
+    constructor(tid300Item, annotation: Types.Annotation) {
+        this.tid300Item = tid300Item;
+        this.annotation = annotation;
+        this.ReferencedSOPSequence = tid300Item.ReferencedSOPSequence;
     }
-    if (handles?.textBox?.hasMoved) {
-      contentEntries.push(this.createQualitativeLabelPosition(this.annotation));
-    }
-    return contentEntries;
-  }
 
-  /**
-   * Remove the incorrect finding sites entry for the text label.
-   * Updated to support the new coding scheme with legacy fallback.
-   */
-  public filterCornerstoneFreeText(contentEntries) {
-    const { codeValues } = Cornerstone3D.CodeScheme;
+    /**
+     * This adds the additional information from the annotation data for
+     * the label data.
+     */
+    public contentItem() {
+        const contentEntries = this.tid300Item.contentItem();
+        const { label, handles } = this.annotation.data;
 
-    const freeTextCodes = [
-      codeValues.FREE_TEXT_CODE_VALUE,
-      codeValues.CORNERSTONEFREETEXT, // legacy support
-    ];
-
-    for (let i = 0; i < contentEntries.length; i++) {
-      const group = contentEntries[i];
-      if (!group.ConceptCodeSequence) {
-        continue;
-      }
-
-      const csLabel = group.ConceptCodeSequence.findIndex((item) =>
-        freeTextCodes.includes(item.CodeValue)
-      );
-
-      if (csLabel !== -1) {
-        group.ConceptCodeSequence.splice(csLabel, 1);
-        if (group.ConceptCodeSequence.length === 0) {
-          contentEntries.splice(i, 1);
+        if (label) {
+            contentEntries.push(this.createQualitativeLabel(label));
+            this.filterCornerstoneFreeText(contentEntries);
         }
-        return;
-      }
+        if (handles?.textBox?.hasMoved) {
+            contentEntries.push(
+                this.createQualitativeLabelPosition(this.annotation)
+            );
+        }
+        return contentEntries;
     }
-  }
 
-  /**
-   * This is the standard TID.1501 method to create a qualitative label
-   * comment.  It returns the DCM:121106 comment code with the text value
-   * contained within the text.  This allows for comments of up to 2^32-4
-   * characters
-   */
-  public createQualitativeLabel(label: string) {
-    const relationshipType = valueTypes.RelationshipTypes.CONTAINS;
-    return new valueTypes.TextContentItem({
-      name: new coding.CodedConcept(COMMENT_CODE),
-      relationshipType,
-      value: label,
-    });
-  }
+    /**
+     * Remove the incorrect finding sites entry for the text label.
+     * Updated to support the new coding scheme with legacy fallback.
+     */
+    public filterCornerstoneFreeText(contentEntries) {
+        const { codeValues } = Cornerstone3D.CodeScheme;
 
-  /**
-   * Creates a qualitative evaluation text label position as an SCOORD (3D)
-   * position labelled with the text annotation position indicator.
-   */
-  public createQualitativeLabelPosition(annotation: Types.Annotation) {
-    const { textBox } = annotation.data.handles;
-    const { referencedImageId, FrameOfReferenceUID: frameOfReferenceUID } =
-      annotation.metadata;
-    const is3DMeasurement = !referencedImageId;
-    const { x, y, z } = toScoord(
-      { is3DMeasurement, referencedImageId },
-      textBox.worldPosition
-    );
+        const freeTextCodes = [
+            codeValues.FREE_TEXT_CODE_VALUE,
+            codeValues.CORNERSTONEFREETEXT // legacy support
+        ];
 
-    const graphicType = valueTypes.GraphicTypes.POINT;
-    const relationshipType = valueTypes.RelationshipTypes.CONTAINS;
-    const name = new coding.CodedConcept(TEXT_ANNOTATION_POSITION);
+        for (let i = 0; i < contentEntries.length; i++) {
+            const group = contentEntries[i];
+            if (!group.ConceptCodeSequence) {
+                continue;
+            }
 
-    const scoord = is3DMeasurement
-      ? new valueTypes.Scoord3DContentItem({
-          name,
-          relationshipType,
-          graphicType,
-          graphicData: [x, y, z],
-          frameOfReferenceUID,
-        })
-      : new valueTypes.ScoordContentItem({
-          name,
-          relationshipType,
-          graphicType,
-          graphicData: [x, y],
+            const csLabel = group.ConceptCodeSequence.findIndex(item =>
+                freeTextCodes.includes(item.CodeValue)
+            );
+
+            if (csLabel !== -1) {
+                group.ConceptCodeSequence.splice(csLabel, 1);
+                if (group.ConceptCodeSequence.length === 0) {
+                    contentEntries.splice(i, 1);
+                }
+                return;
+            }
+        }
+    }
+
+    /**
+     * This is the standard TID.1501 method to create a qualitative label
+     * comment.  It returns the DCM:121106 comment code with the text value
+     * contained within the text.  This allows for comments of up to 2^32-4
+     * characters
+     */
+    public createQualitativeLabel(label: string) {
+        const relationshipType = valueTypes.RelationshipTypes.CONTAINS;
+        return new valueTypes.TextContentItem({
+            name: new coding.CodedConcept(COMMENT_CODE),
+            relationshipType,
+            value: label
         });
+    }
 
-    // Required by TID 1501
-    scoord.ContentSequence = [
-      {
-        RelationshipType: valueTypes.RelationshipTypes.SELECTED_FROM,
-        ValueType: valueTypes.ValueTypes.IMAGE,
-        ReferencedSOPSequence: this.ReferencedSOPSequence,
-      },
-    ];
+    /**
+     * Creates a qualitative evaluation text label position as an SCOORD (3D)
+     * position labelled with the text annotation position indicator.
+     */
+    public createQualitativeLabelPosition(annotation: Types.Annotation) {
+        const { textBox } = annotation.data.handles;
+        const { referencedImageId, FrameOfReferenceUID: frameOfReferenceUID } =
+            annotation.metadata;
+        const is3DMeasurement = !referencedImageId;
+        const { x, y, z } = toScoord(
+            { is3DMeasurement, referencedImageId },
+            textBox.worldPosition
+        );
 
-    return scoord;
-  }
+        const graphicType = valueTypes.GraphicTypes.POINT;
+        const relationshipType = valueTypes.RelationshipTypes.CONTAINS;
+        const name = new coding.CodedConcept(TEXT_ANNOTATION_POSITION);
+
+        const scoord = is3DMeasurement
+            ? new valueTypes.Scoord3DContentItem({
+                  name,
+                  relationshipType,
+                  graphicType,
+                  graphicData: [x, y, z],
+                  frameOfReferenceUID
+              })
+            : new valueTypes.ScoordContentItem({
+                  name,
+                  relationshipType,
+                  graphicType,
+                  graphicData: [x, y]
+              });
+
+        // Required by TID 1501
+        scoord.ContentSequence = [
+            {
+                RelationshipType: valueTypes.RelationshipTypes.SELECTED_FROM,
+                ValueType: valueTypes.ValueTypes.IMAGE,
+                ReferencedSOPSequence: this.ReferencedSOPSequence
+            }
+        ];
+
+        return scoord;
+    }
 }
--- packages/adapters/src/adapters/Cornerstone3D/PlanarFreehandROI.ts
+++ packages/adapters/src/adapters/Cornerstone3D/PlanarFreehandROI.ts
@@ -1,134 +1,137 @@
-import MeasurementReport from './MeasurementReport';
-import { utilities } from 'dcmjs';
-import { vec3 } from 'gl-matrix';
-import BaseAdapter3D from './BaseAdapter3D';
-import { toScoords } from '../helpers';
-import { extractAllNUMGroups, restoreAdditionalMetrics } from './metricHandler';
+import MeasurementReport from "./MeasurementReport";
+import { utilities } from "dcmjs";
+import { vec3 } from "gl-matrix";
+import BaseAdapter3D from "./BaseAdapter3D";
+import { toScoords } from "../helpers";
+import { extractAllNUMGroups, restoreAdditionalMetrics } from "./metricHandler";
 
 const { Polyline: TID300Polyline } = utilities.TID300;
 
 class PlanarFreehandROI extends BaseAdapter3D {
-  public static closedContourThreshold = 1e-5;
+    public static closedContourThreshold = 1e-5;
 
-  static {
-    this.init('PlanarFreehandROI', TID300Polyline);
-  }
-
-  static getMeasurementData(
-    MeasurementGroup,
-    sopInstanceUIDToImageIdMap,
-    metadata
-  ) {
-    const {
-      state,
-      NUMGroup,
-      worldCoords,
-      referencedImageId,
-      ReferencedFrameNumber,
-    } = MeasurementReport.getSetupMeasurementData(
-      MeasurementGroup,
-      sopInstanceUIDToImageIdMap,
-      metadata,
-      this.toolType
-    );
-
-    const distanceBetweenFirstAndLastPoint = vec3.distance(
-      worldCoords[worldCoords.length - 1],
-      worldCoords[0]
-    );
-
-    let isOpenContour = true;
-
-    // If the contour is closed, this should have been encoded as exactly the same point, so check for a very small difference.
-    if (distanceBetweenFirstAndLastPoint < this.closedContourThreshold) {
-      worldCoords.pop(); // Remove the last element which is duplicated.
-
-      isOpenContour = false;
+    static {
+        this.init("PlanarFreehandROI", TID300Polyline);
     }
 
-    const points = [];
+    static getMeasurementData(
+        MeasurementGroup,
+        sopInstanceUIDToImageIdMap,
+        metadata
+    ) {
+        const {
+            state,
+            NUMGroup,
+            worldCoords,
+            referencedImageId,
+            ReferencedFrameNumber
+        } = MeasurementReport.getSetupMeasurementData(
+            MeasurementGroup,
+            sopInstanceUIDToImageIdMap,
+            metadata,
+            this.toolType
+        );
 
-    if (isOpenContour) {
-      points.push(worldCoords[0], worldCoords[worldCoords.length - 1]);
+        const distanceBetweenFirstAndLastPoint = vec3.distance(
+            worldCoords[worldCoords.length - 1],
+            worldCoords[0]
+        );
+
+        let isOpenContour = true;
+
+        // If the contour is closed, this should have been encoded as exactly the same point, so check for a very small difference.
+        if (distanceBetweenFirstAndLastPoint < this.closedContourThreshold) {
+            worldCoords.pop(); // Remove the last element which is duplicated.
+
+            isOpenContour = false;
+        }
+
+        const points = [];
+
+        if (isOpenContour) {
+            points.push(worldCoords[0], worldCoords[worldCoords.length - 1]);
+        }
+        const referencedSOPInstanceUID = state.sopInstanceUid;
+        const allNUMGroups = extractAllNUMGroups(
+            MeasurementGroup,
+            referencedSOPInstanceUID
+        );
+        const measurementNUMGroups =
+            allNUMGroups[referencedSOPInstanceUID] || {};
+        state.annotation.data = {
+            ...state.annotation.data,
+            contour: { polyline: worldCoords, closed: !isOpenContour },
+            handles: {
+                ...state.annotation.data.handles,
+                points
+            },
+            frameNumber: ReferencedFrameNumber
+        };
+
+        if (referencedImageId) {
+            state.annotation.data.cachedStats = {
+                [`imageId:${referencedImageId}`]: {
+                    area: NUMGroup
+                        ? NUMGroup.MeasuredValueSequence.NumericValue
+                        : null,
+                    ...restoreAdditionalMetrics(measurementNUMGroups)
+                }
+            };
+        }
+        return state;
     }
-    const referencedSOPInstanceUID = state.sopInstanceUid;
-    const allNUMGroups = extractAllNUMGroups(
-      MeasurementGroup,
-      referencedSOPInstanceUID
-    );
-    const measurementNUMGroups = allNUMGroups[referencedSOPInstanceUID] || {};
-    state.annotation.data = {
-      ...state.annotation.data,
-      contour: { polyline: worldCoords, closed: !isOpenContour },
-      handles: {
-        ...state.annotation.data.handles,
-        points,
-      },
-      frameNumber: ReferencedFrameNumber,
-    };
 
-    if (referencedImageId) {
-      state.annotation.data.cachedStats = {
-        [`imageId:${referencedImageId}`]: {
-          area: NUMGroup ? NUMGroup.MeasuredValueSequence.NumericValue : null,
-          ...restoreAdditionalMetrics(measurementNUMGroups),
-        },
-      };
+    static getTID300RepresentationArguments(tool, is3DMeasurement = false) {
+        const { data, finding, findingSites, metadata } = tool;
+
+        const { polyline, closed } = data.contour;
+        const isOpenContour = closed !== true;
+
+        const { referencedImageId } = metadata;
+        const scoordProps = {
+            is3DMeasurement,
+            referencedImageId
+        };
+
+        const points = toScoords(scoordProps, polyline);
+
+        if (!isOpenContour) {
+            // Need to repeat the first point at the end of to have an explicitly closed contour.
+            const firstPoint = points[0];
+            points.push(firstPoint);
+        }
+
+        const {
+            area,
+            areaUnit,
+            modalityUnit,
+            perimeter,
+            mean,
+            max,
+            stdDev,
+            length
+        } = data.cachedStats[`imageId:${referencedImageId}`] || {};
+
+        return {
+            /** From cachedStats */
+            points,
+            area,
+            areaUnit,
+            perimeter: perimeter ?? length,
+            modalityUnit,
+            mean,
+            max,
+            stdDev,
+            /** Other */
+            trackingIdentifierTextValue: this.trackingIdentifierTextValue,
+            finding,
+            findingSites: findingSites || [],
+            ReferencedFrameOfReferenceUID: is3DMeasurement
+                ? metadata.FrameOfReferenceUID
+                : null,
+            use3DSpatialCoordinates: is3DMeasurement
+        };
     }
-    return state;
-  }
-
-  static getTID300RepresentationArguments(tool, is3DMeasurement = false) {
-    const { data, finding, findingSites, metadata } = tool;
-
-    const { polyline, closed } = data.contour;
-    const isOpenContour = closed !== true;
-
-    const { referencedImageId } = metadata;
-    const scoordProps = {
-      is3DMeasurement,
-      referencedImageId,
-    };
-
-    const points = toScoords(scoordProps, polyline);
-
-    if (!isOpenContour) {
-      // Need to repeat the first point at the end of to have an explicitly closed contour.
-      const firstPoint = points[0];
-      points.push(firstPoint);
-    }
-
-    const {
-      area,
-      areaUnit,
-      modalityUnit,
-      perimeter,
-      mean,
-      max,
-      stdDev,
-      length,
-    } = data.cachedStats[`imageId:${referencedImageId}`] || {};
-
-    return {
-      /** From cachedStats */
-      points,
-      area,
-      areaUnit,
-      perimeter: perimeter ?? length,
-      modalityUnit,
-      mean,
-      max,
-      stdDev,
-      /** Other */
-      trackingIdentifierTextValue: this.trackingIdentifierTextValue,
-      finding,
-      findingSites: findingSites || [],
-      ReferencedFrameOfReferenceUID: is3DMeasurement
-        ? metadata.FrameOfReferenceUID
-        : null,
-      use3DSpatialCoordinates: is3DMeasurement,
-    };
-  }
 }
 
 export default PlanarFreehandROI;
