75b447f4484c5f859cd1666bb2ebb91932a15c31
Bill Wallace <wayfarer3130@gmail.com>
2026-04-22 08:21:01 -0400

    Export suv scaling factor

--- packages/metadata/src/index.ts
+++ packages/metadata/src/index.ts
@@ -3,5 +3,6 @@ export { version } from './version';
 export * as metaData from './metaData';
 export * as utilities from './utilities';
 export * as logging from './utilities/logging';
+export { calculateSUVScalingFactors } from '@cornerstonejs/calculate-suv';
 export { registerDefaultProvider } from './registerDefaultProvider';
 export type * from './types';
