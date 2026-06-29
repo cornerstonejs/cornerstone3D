56135e6dde92e35db6181d05f8492bad42409c18
Joe Boccanfuso <joe.boccanfuso@radicalimaging.com>
2026-06-29 09:50:18 +0200

    Increase playwright workers to 18.

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   globalSetup: './playwright.globalSetup.ts',
   retries: process.env.CI ? 3 : 0,
-  workers: Number(process.env.PLAYWRIGHT_WORKERS) || 8,
+  workers: Number(process.env.PLAYWRIGHT_WORKERS) || 18,
   timeout: 120 * 1000,
   snapshotPathTemplate:
     'tests/screenshots{/projectName}/{testFilePath}/{arg}{ext}',
