696ad05afd01eac30fc5cc66fa0a92b3e3478164
Bill Wallace <wayfarer3130@gmail.com>
2026-08-11 18:42:49 -0400

    fix(example): Local example was failing to load images (#2854)

--- utils/demo/helpers/convertMultiframeImageIds.js
+++ utils/demo/helpers/convertMultiframeImageIds.js
@@ -1,12 +1,23 @@
-import { metaData } from '@cornerstonejs/core';
-import cornerstoneDICOMImageLoader from '@cornerstonejs/dicom-image-loader';
+import { Enums, imageLoader, metaData } from '@cornerstonejs/core';
+
+const { MetadataModules } = Enums;
+
 /**
  * preloads imageIds metadata in memory
+ *
+ * Loading an imageId through the registered image loader also parses and caches
+ * the instance metadata, which is what the frame expansion below needs in order
+ * to know how many frames an instance has.
  **/
 async function prefetchMetadataInformation(imageIdsToPrefetch) {
   for (let i = 0; i < imageIdsToPrefetch.length; i++) {
-    await cornerstoneDICOMImageLoader.wadouri.loadImage(imageIdsToPrefetch[i])
-      .promise;
+    const imageId = imageIdsToPrefetch[i];
+
+    if (metaData.get(MetadataModules.NATURALIZED, imageId)) {
+      continue;
+    }
+
+    await imageLoader.loadAndCacheImage(imageId);
   }
 }
 
@@ -32,34 +43,57 @@ function getFrameInformation(imageId) {
     };
   }
 }
+
+/**
+ * Expands an imageId using the legacy `multiframeModule` metadata, which is only
+ * registered when the demo runs with the legacy metadata providers.
+ */
+function convertLegacyMultiframeImageId(imageId) {
+  const instanceMetaData = metaData.get(MetadataModules.MULTIFRAME, imageId);
+
+  if (!(instanceMetaData?.NumberOfFrames > 1)) {
+    return [imageId];
+  }
+
+  const { imageIdFrameless } = getFrameInformation(imageId);
+
+  const newImageIds = [];
+  for (let i = 0; i < instanceMetaData.NumberOfFrames; i++) {
+    newImageIds.push(imageIdFrameless + (i + 1));
+  }
+
+  return newImageIds;
+}
+
 /**
  * Receives a list of imageids possibly referring to multiframe dicom images
  * and returns a list of imageid where each imageid referes to one frame.
  * For each imageId representing a multiframe image with n frames,
  * it will create n new imageids, one for each frame, and returns the new list of imageids
  * If a particular imageid no refer to a mutiframe image data, it will be just copied into the new list
+ *
+ * The metadata of the imageIds needs to have been loaded already, see
+ * `prefetchMetadataInformation`.
+ *
  * @returns new list of imageids where each imageid represents a frame
  */
 function convertMultiframeImageIds(imageIds) {
-  const newImageIds = [];
-  imageIds.forEach((imageId) => {
-    const { imageIdFrameless } = getFrameInformation(imageId);
-    const instanceMetaData = metaData.get('multiframeModule', imageId);
-    if (
-      instanceMetaData &&
-      instanceMetaData.NumberOfFrames &&
-      instanceMetaData.NumberOfFrames > 1
-    ) {
-      const NumberOfFrames = instanceMetaData.NumberOfFrames;
-      for (let i = 0; i < NumberOfFrames; i++) {
-        const newImageId = imageIdFrameless + (i + 1);
-        newImageIds.push(newImageId);
-      }
-    } else {
-      newImageIds.push(imageId);
+  return imageIds.flatMap((imageId) => {
+    const baseImageId =
+      metaData.getTyped(MetadataModules.BASE_IMAGE_ID, imageId) ?? imageId;
+    const frameImageIds = metaData.getTyped(
+      MetadataModules.FRAME_IMAGE_IDS,
+      baseImageId
+    );
+
+    if (!frameImageIds) {
+      return convertLegacyMultiframeImageId(imageId);
     }
+
+    // Single frame instances keep their original imageId, only multiframe
+    // instances are expanded into one imageId per frame.
+    return frameImageIds.size > 1 ? [...frameImageIds] : [imageId];
   });
-  return newImageIds;
 }
 
 export { convertMultiframeImageIds, prefetchMetadataInformation };
